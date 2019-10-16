715882fb005df7fe600143dd7c50ee614617ee02
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-10-16 22:19:00 +0530

    Add solution for problem `B - Shooting` Link: https://codeforces.com/contest/1216/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/1216/submission/62728271

--- codeforces/1216/B.cpp
+++ codeforces/1216/B.cpp
@@ -1,36 +1,26 @@
-/**
- 🍪 the_hyp0cr1t3
- 🍪 17.10.2020 14:00:20
-**/
-#ifdef W
-#include "k_II.h"
-#else
 #include <bits/stdc++.h>
 using namespace std;
-#endif
-#define pb emplace_back
-#define sz(x) int(x.size())
-#define all(x) x.begin(), x.end()
+#define long int64_t
 
-const int64_t DESPACITO = 2e18;
-const int INF = 2e9, MOD = 1e9+7;
-const int N = 2e5 + 5;
-
-int32_t main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-    int i, n, ans = 0;
+int main() {
+    int n, i, a, sum = 0;
     cin >> n;
-    vector<pair<int, int>> a;
-    a.reserve(n);
-    for(i = 1; i <= n; i++) {
-        int x; cin >> x;
-        a.pb(x, i);
-    } sort(all(a), greater{});
-
-    for(i = 0; i < n; i++)
-        ans += a[i].first * i + 1;
-
-    cout << ans << '\n';
-    for(auto [x, j]: a)
-        cout << j << ' ';
+    multiset <pair<int,int>> s;
+    for(i = 0; i < n; i++) {
+        cin >> a;
+        s.insert({a, i});
+    }
+    multiset <pair<int,int>> :: iterator itr; 
+    for (itr = prev(s.end(), 1), i = 0;; --itr, i++){
+        sum += 1 + i * itr->first;
+        if  (itr == s.begin())
+            break;
+    }
+    cout << sum << "\n";
+    for (itr = prev(s.end(), 1), i = 0;; --itr, i++) {
+        cout << 1 + itr->second << " ";
+        if  (itr == s.begin())
+            break;
+    }
+    return 0;
 }
