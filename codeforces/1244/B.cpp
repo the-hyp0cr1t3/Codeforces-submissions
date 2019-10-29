b80196d569365e96853a2911d061960b9cdb93af
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-10-29 19:27:00 +0530

    Add solution for problem `B - Rooms and Staircases` Link: https://codeforces.com/contest/1244/problem/B Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/1244/submission/63730730

--- codeforces/1244/B.cpp
+++ codeforces/1244/B.cpp
@@ -1,41 +1,37 @@
 #include <bits/stdc++.h>
-//#define long int64_t
+using namespace std;
+ 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-#define max(a,b) (a>b?a:b)
-#define min(a,b) (a<b?a:b)
-#define MOD 1e8 + 7
-#define INF 2e9
-#define DESPACITO 1e18
-#define PI acos(-1);
-#define E 998244353
-#define pb push_back
-#define mp make_pair
-#define ll long long
+#define int long long
 
-using namespace std;
-const int N = 1e5 + 5;
+const int N = 1e3 + 5;
 
-int main() {
-    int q;
-    cin >> q;
-    while (q--) {
-        int n, i = 1, first = 0, last = 0;
-        bool flag = 0;
-        string s;
-        cin >> n >> s;
-        for (char c: s) {
-            if (c == '1') {
-                last = i;
-                if (!flag)
-                    first = i, flag = 1;
-            }
-            i++;
-        }
-        if (!last)
-            cout << n << endl;
-        else
-            cout << 2 * max(last, n - first + 1) << endl;
-    }
-    return 0;
+int n;
+char s[N];
+
+int32_t main()
+{
+	IOS;
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		cin >> n;
+		int ans = n;
+		int cnt = n;
+		for(int i = 1; i <= n; i++)
+		{
+			cin >> s[i];
+			if(s[i] == '1')
+			{
+				int cur = 2 * i;
+				ans = max(ans, cur);
+				ans = max(ans, 2 * n - cur + 2);
+				cnt++;
+			}
+		}
+		cout << ans << endl;
+	}
+	return 0;
 }
