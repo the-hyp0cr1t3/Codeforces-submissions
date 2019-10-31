89af9b838c0fd018a08aa0e2750bb8e50e8c9b21
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-01 00:26:00 +0530

    Add solution for problem `A - Almost Equal` Link: https://codeforces.com/contest/1205/problem/A Tags: constructive algorithms, greedy, math, *1200 Ref: https://codeforces.com/contest/1205/submission/63945884

--- codeforces/1205/A.cpp
+++ codeforces/1205/A.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
+//#define long int64_t
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define MOD 1e8 + 7
+#define INF 2e9
+#define DESPACITO 1e18
+#define PI acos(-1);
+#define E 998244353
+#define pb push_back
+#define mp make_pair
+#define ll long long
 
 using namespace std;
 const int N = 1e5 + 5;
@@ -7,20 +19,18 @@ const int N = 1e5 + 5;
 int main() {
     int i, n, a[2 * N], k = 1;
     cin >> n;
-    // if n is even, not possible
     if ((n+1) & 1) {
         cout << "NO";
         return 0;
     }
     else 
         cout << "YES" << endl;
-    // a[i] is answer of i-th slot
     for(i = 1; i <= n; i+=2) {
-        a[i] = k++;         // a[i] = k
-        a[n+i] = k++;       // a[n+i] = k+1
-        a[n+i+1] = k++;     // a[n+i+1] = k+2
+        a[i] = k++;
+        a[n+i] = k++;
+        a[n+i+1] = k++;
         if (i < n)
-            a[i+1] = k++;   // a[i+1] = k+3
+            a[i+1] = k++;
     }
     for (i = 1; i <= 2 * n; i++)
         cout << a[i] << " ";
