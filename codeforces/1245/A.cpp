c80afbd991d10102833320d5b70c1298f9b76901
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-01 20:18:00 +0530

    Add solution for problem `A - Good ol' Numbers Coloring` Link: https://codeforces.com/contest/1245/problem/A Tags: math, number theory, *1000 Ref: https://codeforces.com/contest/1245/submission/64000385

---
+++ codeforces/1245/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+//#define long int64_t
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define MOD 1e8 + 7
+#define INF 2e9
+#define DESPACITO 1e18
+#define PI acos(-1);
+#define E 998244353
+#define pb push_back
+#define mp make_pair
+#define ll long long
+
+using namespace std;
+const int N = 1e5 + 5;
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+int main() {
+    int q;
+    cin >> q;
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+        if (gcd(a, b) > 1)
+            cout << "infinite" << endl;
+        else
+            cout << "finite" << endl;
+    }
+    return 0;
+}
