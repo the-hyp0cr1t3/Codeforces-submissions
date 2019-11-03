f57599e0bcc944fdae8894860e3b042e6d8d492f
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-04 00:36:00 +0530

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/8281756

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -1,29 +1,55 @@
 //https://atcoder.jp/contests/dp/tasks/dp_c
 #include <bits/stdc++.h>
+//#define long int64_t
+#define cerr if(0)cerr
+#define watch(x) cerr << (#x) << " is " << (x) << endl
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-using namespace std;
-#define max3(x,y,z) max(x,max(y,z))
+#define endl "\n"
+#define max3(a,b,c) (a>=b and a>=c ? a : b>=c and b>=a ? b : c)
+#define max(a,b) (a>b?a:b)
 #define min(a,b) (a<b?a:b)
-const int N = 1e5 + 5;
-int n, a[N][3], cache[N][3];
+#define MOD 1e8 + 7
+#define INF 2e9
+#define DESPACITO 1e18
+#define PI acos(-1);
+#define E 998244353
+#define pb push_back
+#define mp make_pair
+#define ll long long
 
-int dp (int idx, int opt) {
-    if (idx < 0)
+using namespace std;
+const int N = 1e5 + 5;
+ll a[N], b[N], c[N], cache[N][3];
+
+ll dp (int idx, int opt) {
+    if (idx == 0)
         return 0;
-    int &ans = cache[idx][opt];
-    if (ans != -1)
+    ll &ans = cache[idx][opt];
+    if (ans != -1) 
         return ans;
-    ans = a[idx][opt] + max(dp(idx-1, (opt+1)%3), dp(idx-1, (opt+2)%3));
-    return ans;
+    else {
+        switch(opt) {
+            case 0:
+                ans = max(dp(idx-1, 1), dp(idx-1, 2)) + a[idx];
+                break;
+            case 1:
+                ans = max(dp(idx-1, 0), dp(idx-1, 2)) + b[idx];
+                break;
+            case 2:
+                ans = max(dp(idx-1, 0), dp(idx-1, 1)) + c[idx];
+                break;
+        }
+        //cerr << "cache[" << idx << "][" << opt << "] is " << cache[idx][opt] << endl;
+        return ans;
+    }
 }
 
-int32_t main() {
-    IOS;
-    int i;
+int main() {
+    int i, n;
     memset(cache, -1, sizeof(cache));
     cin >> n;
-    for(i = 0; i < n; i++) 
-        cin >> a[i][0] >> a[i][1] >> a[i][2];
-    cout << max3(dp(n, 0), dp(n, 1), dp(n, 2));
+    for(i = 1; i <= n; i++) 
+        cin >> a[i] >> b[i] >> c[i];
+    cout << max(dp(n, 0), max(dp(n, 1), dp(n, 2)));
     return 0;
 }
