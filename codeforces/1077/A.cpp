31bb1a69d873169b7ff67e4d7131d4455d86bdf4
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-12 01:14:00 +0530

    Add solution for problem `A - Frog Jumping` Link: https://codeforces.com/contest/1077/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1077/submission/64712897

--- codeforces/1077/A.cpp
+++ codeforces/1077/A.cpp
@@ -1,29 +1,42 @@
-/**
- 🍪 the_hyp0cr1t3
- 🍪 01.11.2020 00:52:07
-**/
-#ifdef W
-#include "k_II.h"
-#else
 #include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
 using namespace std;
-#endif
-#define pb emplace_back
-#define sz(x) int(x.size())
-#define all(x) x.begin(), x.end()
-
-const int64_t DESPACITO = 2e18;
-const int INF = 2e9, MOD = 1e9+7;
-const int N = 2e5 + 5;
-
-int the_real_main() {
-    int64_t a, b, k;
-    cin >> a >> b >> k;
-    cout << (k/2*(a-b) + (k&1)*a) << '\n';
-    return 0;
-}
+#define cerr if(0)cerr
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define max3(x,y,z) max(x,max(y,z))
+#define min3(x,y,z) min(x,min(y,z))
+#define MOD 1000000007  //1e9 + 7
+#define INF 2000000000 //2e9
+#define DESPACITO 1000000000000000000 //1e18
+#define PI acos(-1);
+#define E 998244353
+#define ins insert
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)(x).size()
+#define X first
+#define Y second
+#define lb lower_bound
+#define ub upper_bound
+#define ll long long
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int N = 1e5 + 5;
 
 int32_t main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
-    int Q; for(cin >> Q; Q; Q--) the_real_main();
+    IOS;
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        int a, b, k;
+        ll ans = 0;
+        cin >> a >> b >> k;
+        if (k&1) k--, ans += a;
+        for(int i = 0; i < k; i+=2) 
+            ans += a-b;
+        cout << ans << endl;
+    }
+    return 0;
 }
