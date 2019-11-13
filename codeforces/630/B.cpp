9b5a0464d15591a9d1c1f679d927d4f5582f2659
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-13 18:39:00 +0530

    Add solution for problem `B - Moore's Law` Link: https://codeforces.com/contest/630/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/630/submission/64796739

---
+++ codeforces/630/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+using namespace std;
+
+int32_t main() {
+    IOS;
+    double n, t;
+    cin >> n >> t;
+    cout << n * pow(1.000000011, t);
+    return 0;
+}
