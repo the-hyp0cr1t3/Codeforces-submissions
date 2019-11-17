1a686a701aafb387cdb97090efde30f1ef765011
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-17 20:03:00 +0530

    Add solution for problem `C - Letter` Link: https://codeforces.com/contest/180/problem/C Tags: dp, *1400 Ref: https://codeforces.com/contest/180/submission/65259505

--- codeforces/180/C.cpp
+++ codeforces/180/C.cpp
@@ -2,20 +2,29 @@
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 #define min(a,b) (a<b?a:b)
-#define LOW 0
-#define UP 1
+#define INF 2000000000
+#define l first
+#define r second
+typedef pair<int, int> pii;
 const int N = 1e5 + 5;
-int dp[N][2];
+pii dp[N];
 string s;
 
 int32_t main() {
     IOS;
     cin >> s;
-    int n = (int) s.length();
-    for(int i = 1; i <= n; i++) {
-        dp[i][UP] = dp[i-1][UP] + (islower(s[i-1])? 1 : 0);
-        dp[i][LOW] = min(dp[i-1][UP], dp[i-1][LOW]) + (isupper(s[i-1])? 1 : 0);
+    int i, cnt = 0, ans = INF, n = (int) s.length();
+    for(i = 1; i <= n; i++) 
+        cnt += (isupper(s[i-1])? 1 : 0);
+    dp[1] = {0, cnt};
+    for(i = 2; i <= n; i++) {
+        dp[i].l = dp[i-1].l + (islower(s[i-2])? 1 : 0);
+        dp[i].r = dp[i-1].r - (isupper(s[i-2])? 1 : 0) ;
     }
-    cout << min(dp[n][UP], dp[n][LOW]);
+    dp[n+1].l = dp[n].l + (islower(s[n-1])? 1 : 0);
+    dp[n+1].r = 0;
+    for(i = 1; i <= n+1; i++)
+        ans = min(ans, dp[i].l + dp[i].r);
+    cout << ans;
     return 0;
 }
