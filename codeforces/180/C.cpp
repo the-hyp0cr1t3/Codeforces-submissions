c766b14e2499b9317ca42bb32c18ce6ae61854a8
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-17 19:50:00 +0530

    Add solution for problem `C - Letter` Link: https://codeforces.com/contest/180/problem/C Tags: dp, *1400 Ref: https://codeforces.com/contest/180/submission/65258844

--- codeforces/180/C.cpp
+++ codeforces/180/C.cpp
@@ -2,29 +2,30 @@
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 #define min(a,b) (a<b?a:b)
-#define INF 2000000000
-#define l first
-#define r second
-typedef pair<int, int> pii;
+#define LOW 0
+#define UP 1
 const int N = 1e5 + 5;
-pii dp[N];
+int cache[N][2];
 string s;
+
+int dp (int idx, bool opt) {
+    if (idx == 0)
+        return 0;
+    int &ans = cache[idx][opt];
+    if (ans != -1)
+        return ans;
+    if (opt == UP)
+        ans = dp(idx-1, UP) + (islower(s[idx-1])? 1 : 0);
+    else
+        ans = min(dp(idx-1, LOW), dp(idx-1, UP)) + (isupper(s[idx-1])? 1 : 0);
+    return ans;
+}
 
 int32_t main() {
     IOS;
+    memset(cache, -1, sizeof(cache));
     cin >> s;
-    int i, cnt = 0, ans = INF, n = (int) s.length();
-    for(i = 1; i <= n; i++) 
-        cnt += (isupper(s[i-1])? 1 : 0);
-    dp[1] = {0, cnt};
-    for(i = 2; i <= n; i++) {
-        dp[i].l = dp[i-1].l + (islower(s[i-2])? 1 : 0);
-        dp[i].r = dp[i-1].r - (isupper(s[i-2])? 1 : 0) ;
-    }
-    dp[n+1].l = dp[n].l + (islower(s[n-1])? 1 : 0);
-    dp[n+1].r = 0;
-    for(i = 1; i <= n+1; i++)
-        ans = min(ans, dp[i].l + dp[i].r);
-    cout << ans;
+    int n = (int) s.length();
+    cout << min(dp(n, LOW), dp(n, UP));
     return 0;
 }
