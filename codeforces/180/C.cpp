a0998d206180a3f08991804ea9d3a6adafc1d03d
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-17 16:37:00 +0530

    Add solution for problem `C - Letter` Link: https://codeforces.com/contest/180/problem/C Tags: dp, *1400 Ref: https://codeforces.com/contest/180/submission/65248031

--- codeforces/180/C.cpp
+++ codeforces/180/C.cpp
@@ -1,31 +1,15 @@
 #include <bits/stdc++.h>
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-#define min(a,b) (a<b?a:b)
-#define LOW 0
-#define UP 1
-const int N = 1e5 + 5;
-int cache[N][2];
-string s;
-
-int dp (int idx, bool opt) {
-    if (idx == 0)
-        return 0;
-    int &ans = cache[idx][opt];
-    if (ans != -1)
-        return ans;
-    if (opt == UP)
-        ans = dp(idx-1, UP) + (islower(s[idx-1])? 1 : 0);
-    else
-        ans = min(dp(idx-1, LOW), dp(idx-1, UP)) + (isupper(s[idx-1])? 1 : 0);
-    return ans;
-}
-
 int32_t main() {
     IOS;
-    memset(cache, -1, sizeof(cache));
+    int i, cnt = 0, ans = 0;
+    string s;
     cin >> s;
-    int n = (int) s.length();
-    cout << min(dp(n, LOW), dp(n, UP));
+    for(i = 0; i < (int) s.length(); i++) {
+        if (s[i] >= 'a' and s[i] <= 'z') cnt++;
+        else if (cnt) cnt--, ans++;
+    }
+    cout << ans;
     return 0;
 }
