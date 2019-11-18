a39683b9f54024ee468113311c4ff43afa9496a6
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-11-18 18:14:00 +0530

    Add solution for problem `A - Remove a Progression` Link: https://codeforces.com/contest/1194/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1194/submission/65299504

---
+++ codeforces/1194/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+using namespace std;
+int32_t main() {
+    IOS;
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        int n, x;
+        cin >> n >> x;
+        cout << 2*x << endl;
+    }
+    return 0;
+}
