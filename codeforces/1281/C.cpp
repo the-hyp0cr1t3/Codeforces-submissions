59e7258b229ebf7f55c839bc26c3f21b53998144
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-12-16 21:01:00 +0530

    Add solution for problem `C - Cut and Paste` Link: https://codeforces.com/contest/1281/problem/C Tags: implementation, math, *1700 Ref: https://codeforces.com/contest/1281/submission/67029820

--- codeforces/1281/C.cpp
+++ codeforces/1281/C.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 #define MOD 1000000007  //1e9 + 7
 #define sz(x) (int)(x).size()
+const int N = 1e6 + 5;
+ll dp[N];
 int32_t main() {
     IOS;
     int Q;
@@ -13,17 +15,18 @@ int32_t main() {
         int n, i = 0;
         string s;
         cin >> n >> s;
+        dp[0] = sz(s);
         while(sz(s) < n) {
             i++;
             int j = s[i-1]-'1';
             string b = s.substr(i, sz(s)-i);
             while(j--) s += b;
         }
-        ll ans = sz(s);
-        for(i++; i <= n; i++) {
-            ans = ((1ll*(s[i-1]-'0')*(ans-i))%MOD + i + MOD) % MOD;
+        dp[i++] = sz(s);
+        for(; i <= n; i++) {
+            dp[i] = ((1ll*(s[i-1]-'0')*(dp[i-1]-i))%MOD + i + MOD) % MOD;
         }
-        cout << ans << endl;
+        cout << dp[n] << endl;
     }
     return 0;
 }
