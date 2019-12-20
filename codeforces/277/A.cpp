1acc25db4b8213aa33057a63988feb1a1ed88d84
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2019-12-20 15:19:00 +0530

    Add solution for problem `A - Learning Languages` Link: https://codeforces.com/contest/277/problem/A Tags: dfs and similar, dsu, *1400 Ref: https://codeforces.com/contest/277/submission/67274919

--- codeforces/277/A.cpp
+++ codeforces/277/A.cpp
@@ -1,62 +1,44 @@
 #include <bits/stdc++.h>
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
-#define ins insert
+#define rsz resize
+#define pb push_back
 #define sz(a) (int)(a).size()
-const int N = 2000 + 5;
+const int N = 200 + 5;
+int n, m, ans = 0;
+vector<int> g[N];
+vector<bool> vis;
 
-struct dsu {
-    int n, parent[N], node[N];
-    set <int> roots;
-    dsu (int Q) : n(Q) {
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
-            node[i] = 1;
+void makeG() {
+    bool nogood = 1;
+    for(int i = 1; i <= n; i++) {
+        int t, v;
+        cin >> t;
+        if (!t) ans++;
+        while (t--) {
+            nogood = 0;
+            cin >> v;
+            g[m+i].pb(v);
+            g[v].pb(m+i);
         }
     }
-    int findparent (int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = findparent(parent[x]);
-    }
-    void merge (int x, int y) {
-        x = findparent(x);
-        y = findparent(y);
-        if (x == y) return;
-        if (node[x] > node[y])
-            parent[y] = x, node[x] += node[y];
-        else
-            parent[x] = y, node[y] += node[x];
-    }
-    bool same (int x, int y) {
-        return findparent(x) == findparent(y);
-    }
-    bool only (int x) {
-        return node[findparent(x)] == 1;
-    }
-    int CountDisjSets (int start, int end) {
-        for(int i = start; i < end; i++) 
-            roots.ins(findparent(i));
-        return sz(roots);
-    }
-};
+    if (nogood) ans++;
+}
+
+void dfs(int v) {
+    vis[v] = 1;
+    for (auto x: g[v]) 
+        if (!vis[x]) dfs(x);
+}
 
 int32_t main() {
     IOS;
-    int i, n, m, t, v;
+    int i;
     cin >> n >> m;
-    dsu g(n+m+1);
-    bool nogood = 1;
-    for(i = 0; i < n; i++) {
-        cin >> t;
-        while (t--) {
-            nogood = 0;
-            cin >> v;  
-            v--; 
-            g.merge(i, n+v);
-        }
-    }
-    if (nogood) return cout << n, 0;
-    cout << g.CountDisjSets(0, n) - 1;
+    makeG();
+    vis.rsz(n+m+2);
+    for(i = 0; i <= n+m; i++)
+        if (sz(g[i]) and !vis[i]) dfs(i), ans++;
+    cout << ans-1;
     return 0;
 }
