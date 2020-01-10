53aeb8c33c751d8c63c9dc3ef7df09a434d0b956
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-01-10 18:41:00 +0530

    Add solution for problem `A - Plant` Link: https://codeforces.com/contest/185/problem/A Tags: math, *1300 Ref: https://codeforces.com/contest/185/submission/68495149

--- codeforces/185/A.cpp
+++ codeforces/185/A.cpp
@@ -1,183 +1,101 @@
-/**
- *  the_hyp0cr1t3
- *  25.06.2020 03:37:03
-**/
 #include <bits/stdc++.h>
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define int long long
-using namespace std;
-#define e "\n"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-#define Tp template<class T>
-#define Tp2 template<class T1, class T2>
-#define Tps template<class T, class... Ts>
-#define Tps2 template<class T1, class T2, class... Ts>
-#define Tp4 template<class T1, class T2, class T3, class T4>
+using namespace std;
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define max3(x,y,z) max(x,max(y,z))
+#define min3(x,y,z) min(x,min(y,z))
 #define ff first
 #define ss second
-#define rev(Aa) reverse(Aa.begin(),Aa.end())
-#define all(Aa) Aa.begin(),Aa.end()
+#define rev(a) reverse(a.begin(),a.end())
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
 #define lb lower_bound
 #define ub upper_bound
 #define rsz resize
 #define ins insert
-#define mp make_pair
-#define pb emplace_back
-#define pf push_front
-#define popb pop_back
-#define popf pop_front
-#define sz(Xx) (int)(Xx).size()
-typedef long long ll;
-typedef long double ld;
-typedef double db;
-using pii = pair<int, int>;
-const int MOD = 1000000007;  //1e9 + 7
-const int INF = 2000000000;  //2e9
-const ll DESPACITO = 1000000000000000000;  //1e18
-struct num {
-    typedef decay<decltype(MOD)>::type T;
-    T v; 
-    static T inv(T a, T m) { a %= m; return a == 1 ? 1 : (T)(m - ll(inv(m, a)) * ll(m) / a); } 
-    num() : v(0) {}
-    num(ll v_) {
-        v = (-MOD < v_ && v_ < MOD) ? T(v_) : T(v_ % MOD);
-        if (v < 0) v += MOD;
-    }
-    explicit operator T() const { return v; }
-    friend std::ostream& operator << (std::ostream& out, const num& n) { return out << T(n); }
-    friend std::istream& operator >> (std::istream& in, num& n) { ll v_; in >> v_; n = num(v_); return in; } 
-    friend bool operator == (const num& a, const num& b) { return a.v == b.v; }
-    friend bool operator != (const num& a, const num& b) { return a.v != b.v; }
-    num inv() const { num res; res.v = inv(v, MOD); return res; }
-    num operator- () const { num res; res.v = v ? MOD-v : 0; return res; }
-    num operator+ () const { return num(*this); }
-    num& operator ++ () { v ++; if (v == MOD) v = 0; return *this; }
-    num& operator -- () { if (v == 0) v = MOD; v --; return *this; }
-    num& operator += (const num& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
-    num& operator -= (const num& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
-    num& operator *= (const num& o) { v = T(ll(v) * ll(o.v) % MOD); return *this; }
-    num& operator /= (const num& o) { return *this *= o.inv(); }
-    // friend num operator ++ (num& a, int) { num r = a; ++a; return r; }
-    // friend num operator -- (num& a, int) { num r = a; --a; return r; }
-    friend num operator + (const num& a, const num& b) { return num(a) += b; }
-    friend num operator - (const num& a, const num& b) { return num(a) -= b; }
-    friend num operator * (const num& a, const num& b) { return num(a) *= b; }
-    friend num operator / (const num& a, const num& b) { return num(a) /= b; }
-    friend bool operator < (const num& a, const num& b) { return a.v < b.v; }
-    friend bool operator <= (const num& a, const num& b) { return a.v <= b.v; }
-    friend bool operator > (const num& a, const num& b) { return a.v > b.v; }
-    friend bool operator >= (const num& a, const num& b) { return a.v >= b.v; }
-};
-
-namespace minmax {
-    Tp T maxx(const T& A) { return A; }
-    Tp T minn(const T& A) { return A; }
-    Tp2 T1 maxx(const T1& A, const T2& B) { return A>B?A:B; }
-    Tp2 T1 minn(const T1& A, const T2& B) { return A<B?A:B; }
-    Tps T maxx(const T& A, const Ts&... ts) { T B = maxx(ts...); return A>B?A:B; }
-    Tps T minn(const T& A, const Ts&... ts) { T B = minn(ts...); return A<B?A:B; }
-    Tps T chmax(T& A, const Ts&... ts) { A = maxx(A, ts...); return A; }
-    Tps T chmin(T& A, const Ts&... ts) { A = minn(A, ts...); return A; }
-    Tp4 void chmax2(T1& A, T2& Aa, const T3& B, const T4& Bb) { if(B > A) A = B, Aa = Bb; }
-    Tp4 void chmin2(T1& A, T2& Aa, const T3& B, const T4& Bb) { if(B < A) A = B, Aa = Bb; }
-}
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define MOD 1000000007  //1e9 + 7
+#define INF 2000000000 //2e9
+#define DESPACITO 1000000000000000000 //1e18
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<class T> T chmin(T &a, T b) { a = min(a, b); return a; }
+template<class T> T chmax(T &a, T b) { a = max(a, b); return a; }
 namespace input { 
-    Tp void re(T&& Xx) { cin >> Xx; }
-    Tp2 void re(pair<T1,T2>& Pp) { re(Pp.first); re(Pp.second); }
-    Tp void re(vector<T>& Aa) { for(int i = 0; i < sz(Aa); i++) re(Aa[i]); }
-    Tp2 void rea(T1&& Aa, T2 t) { for(int i = 0; i < t; i++) re(Aa[i]); }
-    Tps2 void rea(T1&& Aa, T2 t, Ts&&... ts) { rea(Aa, t); rea(ts...); }
-    Tp2 void rea1(T1&& Aa, T2 t) { for(int i = 1; i <= t; i++) re(Aa[i]); }
-    Tps2 void rea1(T1&& Aa, T2 t, Ts&... ts) { rea1(Aa, t); rea1(ts...); }
-    Tps void re(T&& t, Ts&... ts) { re(t); re(ts...); }
+    template<typename T> void re(T&& x) { cin >> x; }
+    template<typename T1, typename T2> void re(pair<T1,T2>& p) { re(p.ff); re(p.ss); }
+    template<typename T> void re(vector<T>& a) { for(int i = 0; i < sz(a); i++) re(a[i]); }
+    template<typename T, typename Tsize> void rea(T&& a, Tsize t) { for(int i = 0; i < t; i++) re(a[i]); }
+    template<typename T, typename... Ts> void re(T&& t, Ts&... ts) { re(t); re(ts...); }
 }
 namespace output {
-    void pr(int32_t Xx) { cout << Xx; }
-    void pr(num Xx) { cout << Xx; }
-    void pr(bool Xx) { cout << Xx; }
-    void pr(long long Xx) { cout << Xx; }
-    void pr(long long unsigned Xx) { cout << Xx; }
-    void pr(double Xx) { cout << Xx; }
-    void pr(char Xx) { cout << Xx; }
-    void pr(const string& Xx) { cout << Xx; }
-    void pr(const char* Xx) { cout << Xx; }
-    void pr(const char* Xx, size_t len) { cout << string(Xx, len); }
-    void ps() { cout << endl; }
-    void pn() { /*do nothing*/ }
+    void pr(int x) { cout << x; }
+    void pr(bool x) { cout << x; }
+    void pr(long long x) { cout << x; }
+    void pr(double x) { cout << x; }
+    void pr(char x) { cout << x; }
+    void pr(const string& x) { cout << x; }
+    void pr(const char* x) { cout << x; }
+    void pr(const char* x, size_t len) { cout << string(x, len); }
+    void ps() { pr("\n"); }
     void pw() { pr(" "); }
-    void pc() { pr("]"); ps(); }
-    Tp2 void pr(const pair<T1,T2>& Xx) { pr(Xx.first); pw(); pr(Xx.second);}
-    Tp void pr(const T&);
-    bool parse(const char* t) { if(t == e) return true; return false;}
-    Tp bool parse(T&& t) { return false;}
-    Tp2 bool parsepair(const pair<T1,T2>& Xx) { return true; }
-    Tp bool parsepair(T&& t) { return false;}
-    Tp2 void psa(T1&& Aa, T2 t) { for(int i = 0; i < t; i++) pr(Aa[i]), pw(); ps(); }
-    Tp2 void pna(T1&& Aa, T2 t) { for(int i = 0; i < t; i++) pr(Aa[i]), ps(); }
-    Tp2 void psa2(T1&& Aa, T2 t1, T2 t2) { for(int i = 0; i < t1; i++) {for(int j = 0; j < t2; j++) pr(Aa[i][j]), pw(); ps();} }
-    Tp void pr(const T& Xx) { if(!sz(Xx)) return; bool fst = 1; bool op = 0; if (parsepair(*Xx.begin())) op = 1; for (const auto& Aa: Xx) {if(!fst) pw(); if(op) pr("{"); pr(Aa), fst = 0; if(op) pr("}"); } }
-    Tps void pr(const T& t, const Ts&... ts) { pr(t); pr(ts...); }
-    Tps void ps(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) {if (!parse(t)) pw(); } ps(ts...); }
-    Tp void pn(const T& t) { for (const auto& Aa: t) ps(Aa); }
-    Tps void pw(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pw(); pw(ts...); }
-    Tps void pc(const T& t, const Ts&... ts) { bool op = 0; if (parsepair(t)) op = 1; if(op) pr("{"); pr(t); if(op) pr("}"); if (sizeof...(ts)) pr(", "); pc(ts...); }
+    void pc() { pr("]\n"); }
+    template<typename T, typename Tsize> void psa(T&& a, Tsize t) { for(int i = 0; i < t; i++) pr(a[i]), pr(" "); ps(); }
+    template<typename T, typename Tsize> void psa2(T&& a, Tsize t1, Tsize t2) { for(int i = 0; i < t1; i++) {for(int j = 0; j < t2; j++) pr(a[i][j]), pr(" "); ps();} }
+    template<typename T1, typename T2> void pr(const pair<T1,T2>& x) { pr("{"); pr(x.ff); pr(", "); pr(x.ss); pr("}"); }
+    template<typename T> void pr(const T& x) { pr("{"); bool fst = 1; for (const auto& a: x) pr(!fst?", ":""), pr(a), fst = 0; pr("}"); }
+    template<typename T, typename... Ts> void pr(const T& t, const Ts&... ts) { pr(t); pr(ts...); }
+    template<typename T, typename... Ts> void ps(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); }
+    template<typename T, typename... Ts> void pw(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(" "); pw(ts...); }
+    template<typename T, typename... Ts> void pc(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); }
     namespace trace {
-        #define tr(Xx...) pr("[",#Xx,"] = ["), pc(Xx);
-        #define tra(Xx, y...) __f0(#Xx, Xx, y)
-        #define tran(Xx, n) __fn(n, #Xx, Xx) // TO DO~ variadic multidimensional
-        Tp2 void __f(const char* name, const T1& Xx, const T2& y){ pr("[",y,"] = "); ps(Xx); }
-        Tps2 void __f(const char* name, const T1& Xx, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr("[",y,"]"); __f(open+1, Xx, rest...); }
-        Tps2 void __f0(const char* name, const T1& Xx, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr(name, size_t(open-name)); __f(name, Xx, y, rest...); }
-        Tp void __fn(int n, const char* name, const T& Xx) { for(int i = 0; i < n; i++) pr(name), __f(name, Xx[i], i); }
+        #define tr(x...) pr("[",#x,"] = ["), pc(x);
+        #define tra(x, y...) __f0(#x, x, y)
+        #define tran(n, x) __fn(n, #x, x) // TO DO~ variadic multidimensional
+        template <typename T1, typename T2> void __f(const char* name, const T1& x, const T2& y){ pr("[",y,"] = "); ps(x); }
+        template <typename T1, typename T2, typename... Ts> void __f(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr("[",y,"]"); __f(open+1, x, rest...); }
+        template <typename T1, typename T2, typename... Ts> void __f0(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr(name, size_t(open-name)); __f(name, x, y, rest...); }
+        template <typename T> void __fn(int n, const char* name, const T& x) { for(int i = 0; i < n; i++) pr(name), __f(name, x[i], i); }
     }
 }
-using namespace minmax;
 using namespace input;
 using namespace output;
 using namespace output::trace;
-//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+using pii = pair<int, int>;
+using ll = long long;
 const int N = 2e5 + 5;
-template<typename T>
-using matrix = vector<vector<T>>;
 
-template<typename T>
-matrix<T> operator*(const matrix<T>& A, const matrix<T>& B) {
-    int _n = sz(A); int _m = sz(B[0]); int _k = sz(B);
-    matrix<T> C(_n, vector<T>(_m));
-    for(int i = 0; i < _n; i++)
-        for(int j = 0; j < _m; j++)
-            for(int k = 0; k < _k; k++)
-                C[i][j] += A[i][k] * B[k][j];
+typedef vector<vector<int>> matrix;
+const int MATN = 2;
+
+matrix mul(matrix A, matrix B) {
+    matrix C(MATN, vector<int>(MATN));
+    for (int i = 0; i < MATN; i++)
+        for (int j = 0; j < MATN; j++)
+            for (int k = 0; k < MATN; k++)
+                C[i][j] = (int)((1ll * A[i][k] * B[k][j] % MOD + C[i][j]) % MOD);
     return C;
 }
 
-template<typename T>
-matrix<T> expo(matrix<T> A, int B) {
-    assert(B >= 0);
-    int _n = sz(A);
-    matrix<T> res(_n, vector<T>(_n));
-    for(int i = 0; i < _n; i++)
-        res[i][i] = 1;
-    while(B > 0) {
-        if(B & 1) res = res * A;
-        A = A * A;
-        B >>= 1;
-    }
-    return res;
-};
+matrix pow(matrix A, ll p) {
+    if (p == 1)
+        return A;
+    if (p & 1)
+        return mul(A, pow(A, p-1));
+    matrix X = pow(A, p/2);
+    return mul(X, X);
+}
 
 int32_t main() {
     IOS;
-    int i, n;
+    int i;
+    ll n;
     re(n);
-    if(!n) return ps(1), 0;
-    matrix<num> F{{3, 1}, {1, 3}};
-    auto res = expo(F, n);
-    ps(res[0][0]);
+    if (!n) return ps(1), 0;
+    matrix F = {{3, 1}, {1, 3}};
+    F = pow(F, n);
+    ps(F[0][0]);
     return 0;
 }
-
-/**
-    Looe but dRnat tduch
-
-**/
