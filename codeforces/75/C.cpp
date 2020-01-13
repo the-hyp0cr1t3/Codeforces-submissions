df6b1a380cc20f034379710c305d86bc10d12750
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-01-13 19:38:00 +0530

    Add solution for problem `C - Modified GCD` Link: https://codeforces.com/contest/75/problem/C Tags: binary search, number theory, *1600 Ref: https://codeforces.com/contest/75/submission/68714853

---
+++ codeforces/75/C.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define e "\n"
+using namespace std;
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define max3(x,y,z) max(x,max(y,z))
+#define min3(x,y,z) min(x,min(y,z))
+#define ff first
+#define ss second
+#define rev(a) reverse(a.begin(),a.end())
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define lb lower_bound
+#define ub upper_bound
+#define rsz resize
+#define ins insert
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define MOD 1000000007  //1e9 + 7
+#define INF 2000000000 //2e9
+#define DESPACITO 1000000000000000000 //1e18
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<class T> T chmin(T &a, T b) { a = min(a, b); return a; }
+template<class T> T chmax(T &a, T b) { a = max(a, b); return a; }
+namespace input { 
+    template<typename T> void re(T&& x) { cin >> x; }
+    template<typename T1, typename T2> void re(pair<T1,T2>& p) { re(p.ff); re(p.ss); }
+    template<typename T> void re(vector<T>& a) { for(int i = 0; i < sz(a); i++) re(a[i]); }
+    template<typename T, typename Tsize> void rea(T&& a, Tsize t) { for(int i = 0; i < t; i++) re(a[i]); }
+    template<typename T, typename Tsize> void rea1(T&& a, Tsize t) { for(int i = 1; i <= t; i++) re(a[i]); }
+    template<typename T, typename... Ts> void re(T&& t, Ts&... ts) { re(t); re(ts...); }
+}
+namespace output {
+    void pr(int x) { cout << x; }
+    void pr(bool x) { cout << x; }
+    void pr(long long x) { cout << x; }
+    void pr(double x) { cout << x; }
+    void pr(char x) { cout << x; }
+    void pr(const string& x) { cout << x; }
+    void pr(const char* x) { cout << x; }
+    void pr(const char* x, size_t len) { cout << string(x, len); }
+    void ps() { pr("\n"); }
+    void pw() { pr(" "); }
+    void pc() { pr("]\n"); }
+    bool parse(const char* t) { if(t == e) return true; return false;}
+    template<typename T> bool parse(T&& t) { return false;}
+    template<typename T, typename Tsize> void psa(T&& a, Tsize t) { for(int i = 0; i < t; i++) pr(a[i]), pr(" "); ps(); }
+    template<typename T, typename Tsize> void psa2(T&& a, Tsize t1, Tsize t2) { for(int i = 0; i < t1; i++) {for(int j = 0; j < t2; j++) pr(a[i][j]), pr(" "); ps();} }
+    template<typename T1, typename T2> void pr(const pair<T1,T2>& x) { pr("{"); pr(x.ff); pr(", "); pr(x.ss); pr("}"); }
+    template<typename T> void pr(const T& x) { pr("{"); bool fst = 1; for (const auto& a: x) pr(!fst?", ":""), pr(a), fst = 0; pr("}"); }
+    template<typename T, typename... Ts> void pr(const T& t, const Ts&... ts) { pr(t); pr(ts...); }
+    template<typename T, typename... Ts> void ps(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) {if (!parse(t)) pr(" ");} ps(ts...); }
+    template<typename T, typename... Ts> void pw(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(" "); pw(ts...); }
+    template<typename T, typename... Ts> void pc(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); }
+    namespace trace {
+        #define tr(x...) pr("[",#x,"] = ["), pc(x);
+        #define tra(x, y...) __f0(#x, x, y)
+        #define tran(n, x) __fn(n, #x, x) // TO DO~ variadic multidimensional
+        template <typename T1, typename T2> void __f(const char* name, const T1& x, const T2& y){ pr("[",y,"] = "); ps(x); }
+        template <typename T1, typename T2, typename... Ts> void __f(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr("[",y,"]"); __f(open+1, x, rest...); }
+        template <typename T1, typename T2, typename... Ts> void __f0(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr(name, size_t(open-name)); __f(name, x, y, rest...); }
+        template <typename T> void __fn(int n, const char* name, const T& x) { for(int i = 0; i < n; i++) pr(name), __f(name, x[i], i); }
+    }
+}
+using namespace input;
+using namespace output;
+using namespace output::trace;
+using pii = pair<int, int>;
+using ll = long long;
+const int N = 2e5 + 5;
+set <int> s;
+
+int solve () {
+    int l, r; re(l, r);
+    int ans = -1;
+    auto it = s.lb(l);
+    if (it == s.end() or *it > r) return ps(-1), 0;
+    while(it != s.end() and *it <= r) ans = *it, it = s.ub(*it);
+    ps(ans);
+    return 0;
+}
+
+int32_t main() {
+    IOS;
+    int i, a, b, c, Q;
+    re(a, b);
+    c = __gcd(a, b);
+    for(i = 1; i*i <= c; i++) 
+        if (c%i == 0) s.ins(i), s.ins(c/i);
+    for(cin >> Q; Q; Q--)
+        solve();
+    return 0;
+}
+//is a bruteforce possible?
+//think greedier, make more assumptions
+// read complete input, be careful if breaking in between
+//stuck for more than 5 minutes? move on
