1c8de22b96dd885be511e830904b5979c4aa3172
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-01-17 17:35:00 +0530

    Add solution for problem `C - News Distribution` Link: https://codeforces.com/contest/1167/problem/C Tags: dfs and similar, dsu, graphs, *1400 Ref: https://codeforces.com/contest/1167/submission/68984740

--- codeforces/1167/C.cpp
+++ codeforces/1167/C.cpp
@@ -1,161 +1,137 @@
 #include <bits/stdc++.h>
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-// #define int long long
-using namespace std;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define e "\n"
-#define endl "\n"
+using namespace std;
 #define Tp template<class T>
 #define Tp2 template<class T1, class T2>
 #define Tps template<class T, class... Ts>
 #define Tps2 template<class T1, class T2, class... Ts>
 #define ff first
 #define ss second
-#define rev(Aa) reverse(Aa.begin(),Aa.end())
-#define all(Aa) Aa.begin(),Aa.end()
+#define rev(a) reverse(a.begin(),a.end())
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
 #define lb lower_bound
 #define ub upper_bound
 #define rsz resize
 #define ins insert
-#define mp make_pair
-#define pb emplace_back
-#define pf push_front
-#define popb pop_back
-#define popf pop_front
-#define sz(Xx) (int)(Xx).size()
-typedef long long ll;
-typedef long double ld;
-typedef double db;
-using pii = pair<int, int>;
-ll MOD = 1000000007;  //1e9 + 7
-const int INF = 2000000000;  //2e9
-const ll DESPACITO = 1000000000000000000;  //1e18
-namespace minmax {
-    Tp T max(T&& A) { return A; }
-    Tp T min(T&& A) { return A; }
-    Tp T max(T&& A, T&& B) { return A>B?A:B; }
-    Tp T chmin2(T&& A, T&& B) { return A<B?A:B; }
-    Tps T max(T&& A, Ts&&... ts) { T B = max(ts...); return A>B?A:B; }
-    Tps T min(T&& A, Ts&&... ts) { T B = min(ts...); return A<B?A:B; }
-    Tps T chmax(T&& A, Ts&&... ts) { A = max(A, ts...); return A; }
-    Tps T chmin(T&& A, Ts&&... ts) { A = min(A, ts...); return A; }
-    Tp2 void chmin2(T1&& A, T2&& Aa, T1&& B, T2&& Bb) { if(B < A) A = B, Aa = Bb; }
-    Tp2 void chmax2(T1&& A, T2&& Aa, T1&& B, T2&& Bb) { if(B > A) A = B, Aa = Bb; }
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define MOD 1000000007  //1e9 + 7
+#define INF 2000000000 //2e9
+#define DESPACITO 1000000000000000000 //1e18
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+namespace maxmin {
+    Tp T max (T&& a) { return a; }
+    Tp T min (T&& a) { return a; }
+    Tp T max (T&& a, T&& b) { return a>b?a:b; }
+    Tp T min (T&& a, T&& b) { return a<b?a:b; }
+    Tps T max (T&& a, Ts&&... ts) { T b = max(ts...); return a>b?a:b; }
+    Tps T min (T&& a, Ts&&... ts) { T b = min(ts...); return a<b?a:b; }
+    Tps T chmax(T&& a, Ts&&... ts) { a = max(a, ts...); return a; }
+    Tps T chmin(T&& a, Ts&&... ts) { a = min(a, ts...); return a; }
 }
 namespace input { 
-    Tp void re(T&& Xx) { cin >> Xx; }
-    Tp2 void re(pair<T1,T2>& Pp) { re(Pp.first); re(Pp.second); }
-    Tp void re(vector<T>& Aa) { for(int i = 0; i < sz(Aa); i++) re(Aa[i]); }
-    Tp2 void rea(T1&& Aa, T2 t) { for(int i = 0; i < t; i++) re(Aa[i]); }
-    Tps2 void rea(T1&& Aa, T2 t, Ts&&... ts) { rea(Aa, t); rea(ts...); }
-    Tp2 void rea1(T1&& Aa, T2 t) { for(int i = 1; i <= t; i++) re(Aa[i]); }
-    Tps2 void rea1(T1&& Aa, T2 t, Ts&... ts) { rea1(Aa, t); rea1(ts...); }
+    Tp void re(T&& x) { cin >> x; }
+    Tp2 void re(pair<T1,T2>& p) { re(p.ff); re(p.ss); }
+    Tp void re(vector<T>& a) { for(int i = 0; i < sz(a); i++) re(a[i]); }
+    Tp2 void rea(T1&& a, T2 t) { for(int i = 0; i < t; i++) re(a[i]); }
+    Tp2 void rea1(T1&& a, T2 t) { for(int i = 1; i <= t; i++) re(a[i]); }
     Tps void re(T&& t, Ts&... ts) { re(t); re(ts...); }
 }
 namespace output {
-    void pr(int Xx) { cout << Xx; }
-    // void pr(num Xx) { cout << Xx; }
-    void pr(bool Xx) { cout << Xx; }
-    void pr(long long Xx) { cout << Xx; }
-    void pr(long long unsigned Xx) { cout << Xx; }
-    void pr(double Xx) { cout << Xx; }
-    void pr(char Xx) { cout << Xx; }
-    void pr(const string& Xx) { cout << Xx; }
-    void pr(const char* Xx) { cout << Xx; }
-    void pr(const char* Xx, size_t len) { cout << string(Xx, len); }
-    void ps() { cout << endl; }
-    void pn() { /*do nothing*/ }
+    void pr(int x) { cout << x; }
+    //void pr(num x) { cout << x; }
+    void pr(bool x) { cout << x; }
+    void pr(long long x) { cout << x; }
+    void pr(long long unsigned x) { cout << x; }
+    void pr(double x) { cout << x; }
+    void pr(char x) { cout << x; }
+    void pr(const string& x) { cout << x; }
+    void pr(const char* x) { cout << x; }
+    void pr(const char* x, size_t len) { cout << string(x, len); }
+    void ps() { pr("\n"); }
     void pw() { pr(" "); }
-    void pc() { pr("]"); ps(); }
-    Tp2 void pr(const pair<T1,T2>& Xx) { pr(Xx.first); pw(); pr(Xx.second);}
-    Tp void pr(const T&);
+    void pc() { pr("]\n"); }
     bool parse(const char* t) { if(t == e) return true; return false;}
+    Tp void pr(const T&);
     Tp bool parse(T&& t) { return false;}
-    Tp2 bool parsepair(const pair<T1,T2>& Xx) { return true; }
-    Tp bool parsepair(T&& t) { return false;}
-    Tp2 void psa(T1&& Aa, T2 t) { for(int i = 0; i < t; i++) pr(Aa[i]), pw(); ps(); }
-    Tp2 void pna(T1&& Aa, T2 t) { for(int i = 0; i < t; i++) pr(Aa[i]), ps(); }
-    Tp2 void psa2(T1&& Aa, T2 t1, T2 t2) { for(int i = 0; i < t1; i++) {for(int j = 0; j < t2; j++) pr(Aa[i][j]), pw(); ps();} }
-    Tp void pr(const T& Xx) { if(!sz(Xx)) return; bool fst = 1; bool op = 0; if (parsepair(*Xx.begin())) op = 1; for (const auto& Aa: Xx) {if(!fst) pw(); if(op) pr("{"); pr(Aa), fst = 0; if(op) pr("}"); } }
+    Tp2 void psa(T1&& a, T2 t) { for(int i = 0; i < t; i++) pr(a[i]), pr(" "); ps(); }
+    Tp2 void psa2(T1&& a, T2 t1, T2 t2) { for(int i = 0; i < t1; i++) {for(int j = 0; j < t2; j++) pr(a[i][j]), pr(" "); ps();} }
+    Tp2 void pr(const pair<T1,T2>& x) { pr("{"); pr(x.ff); pr(", "); pr(x.ss); pr("}"); }
+    Tp void pr(const T& x) { pr("{"); bool fst = 1; for (const auto& a: x) pr(!fst?", ":""), pr(a), fst = 0; pr("}"); }
     Tps void pr(const T& t, const Ts&... ts) { pr(t); pr(ts...); }
-    Tps void ps(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) {if (!parse(t)) pw(); } ps(ts...); }
-    Tp void pn(const T& t) { for (const auto& Aa: t) ps(Aa); }
-    Tps void pw(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pw(); pw(ts...); }
-    Tps void pc(const T& t, const Ts&... ts) { bool op = 0; if (parsepair(t)) op = 1; if(op) pr("{"); pr(t); if(op) pr("}"); if (sizeof...(ts)) pr(", "); pc(ts...); }
+    Tps void ps(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) {if (!parse(t)) pr(" "); } ps(ts...); }
+    Tp void pwv(const T& x) { for (const auto& a: x) pr(a), pw(); ps(); }
+    Tps void pw(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(" "); pw(ts...); }
+    Tps void pc(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); }
     namespace trace {
-        #define tr(Xx...) pr("[",#Xx,"] = ["), pc(Xx);
-        #define tra(Xx, y...) __f0(#Xx, Xx, y)
-        #define tran(Xx, n) __fn(n, #Xx, Xx) // TO DO~ variadic multidimensional
-        Tp2 void __f(const char* name, const T1& Xx, const T2& y){ pr("[",y,"] = "); ps(Xx); }
-        Tps2 void __f(const char* name, const T1& Xx, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr("[",y,"]"); __f(open+1, Xx, rest...); }
-        Tps2 void __f0(const char* name, const T1& Xx, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr(name, size_t(open-name)); __f(name, Xx, y, rest...); }
-        Tp void __fn(int n, const char* name, const T& Xx) { for(int i = 0; i < n; i++) pr(name), __f(name, Xx[i], i); }
+        #define tr(x...) pr("[",#x,"] = ["), pc(x);
+        #define tra(x, y...) __f0(#x, x, y)
+        #define tran(n, x) __fn(n, #x, x) // TO DO~ variadic multidimensional
+        Tp2 void __f(const char* name, const T1& x, const T2& y){ pr("[",y,"] = "); ps(x); }
+        Tps2 void __f(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr("[",y,"]"); __f(open+1, x, rest...); }
+        Tps2 void __f0(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr(name, size_t(open-name)); __f(name, x, y, rest...); }
+        Tp void __fn(int n, const char* name, const T& x) { for(int i = 0; i < n; i++) pr(name), __f(name, x[i], i); }
     }
 }
-using namespace minmax;
+using namespace maxmin;
 using namespace input;
 using namespace output;
 using namespace output::trace;
-//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+using pii = pair<int, int>;
+using ll = long long;
 const int N = 5e5 + 5;
-namespace DSU {
-    int nn, par[N], size_[N], roots[N], rootID[N], rootcnt;
-    void build (int Q) {
-        nn = Q;
-        rootcnt = nn;
-        for(int i = 0; i < nn; i++) {
-            par[i] = i;
-            size_[i] = 1;
+struct DSU {
+    int n, parent[N], node[N], roots[N], rootID[N], rootcnt;
+    DSU (int Q) : n(Q) {
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            node[i] = 1;
+            rootcnt = 0;
         }
     }
-    int getpar(int x) {
-        int p = x;
-        while(par[p] != p)
-            p = par[p];
-        while(par[x] != x) {
-            int z = par[x];
-            par[x] = p;
-            x = z;
-        }
-        return p;
+    int findparent (int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = findparent(parent[x]);
     }
-    void merge(int x, int y) {
-        x = getpar(x);
-        y = getpar(y);
+    void merge (int x, int y) {
+        x = findparent(x);
+        y = findparent(y);
         if (x == y) return;
-        if (size_[x] < size_[y]) swap(x, y);
-        par[y] = x, size_[x] += size_[y];
-        rootcnt--;
+        if (node[x] < node[y]) swap(x, y);
+        parent[y] = x, node[x] += node[y];
     }
-    bool same(int x, int y) {
-        return getpar(x) == getpar(y);
+    bool same (int x, int y) {
+        return findparent(x) == findparent(y);
     }
-    void getRoots() {
-        int cnt = 0;
-        for(int i = 0; i < nn; i++) {
-            if (par[i] != i) continue;
-            rootID[i] = cnt;
-            roots[cnt++] = i;
+    bool only (int x) {
+        return node[findparent(x)] == 1;
+    }
+    void getRoots () {
+        for(int i = 0; i < n; i++) {
+            if (parent[i] != i) continue;
+            rootID[i] = rootcnt;
+            roots[rootcnt++] = i;
         }
-        assert(cnt == rootcnt);
     }
-}
-using namespace DSU;
-
+};
 int32_t main() {
     IOS;
     int i, n, m;
     re(n, m);
-    build(n);
+    DSU g(n);
     while (m--) {
         int u, v, k; re(k);
         if (!k) continue;
         re(u); --u; --k;
         while (k--) {
             re(v); --v;
-            merge(u, v);
+            g.merge(u, v);
         }
     }
     for(i = 0; i < n; i++) 
-        pw(size_[getpar(i)]);
+        pw(g.node[g.findparent(i)]);
     return 0;
 }
