ff3d114c69bad2027965f985123fa70171815104
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-01-29 17:56:00 +0530

    Add solution for problem `C - Books Queries` Link: https://codeforces.com/contest/1066/problem/C Tags: implementation, *1400 Ref: https://codeforces.com/contest/1066/submission/69726505

--- codeforces/1066/C.cpp
+++ codeforces/1066/C.cpp
@@ -1,90 +1,110 @@
-/**
- 🍪 the_hyp0cr1t3
- 🍪 04.08.2020 23:06:30
-**/
 #include <bits/stdc++.h>
-// #define int long long
-#define k_II ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define W return 0;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define e "\n"
 #define endl "\n"
+using namespace std;
+#define Tp template<class T>
+#define Tp2 template<class T1, class T2>
+#define Tps template<class T, class... Ts>
+#define Tps2 template<class T1, class T2, class... Ts>
 #define ff first
 #define ss second
-#define rev(Aa) reverse(Aa.begin(),Aa.end())
-#define all(Aa) Aa.begin(),Aa.end()
+#define rev(a) reverse(a.begin(),a.end())
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define lb lower_bound
+#define ub upper_bound
+#define rsz resize
 #define ins insert
 #define mp make_pair
-#define pb emplace_back
-#define sz(Xx) (int)(Xx).size()
-
-using namespace std;
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define MOD 1000000007  //1e9 + 7
+#define INF 2000000000 //2e9
+#define DESPACITO 1000000000000000000 //1e18
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+namespace maxmin {
+    Tp T max (T&& a) { return a; }
+    Tp T min (T&& a) { return a; }
+    Tp T max (T&& a, T&& b) { return a>b?a:b; }
+    Tp T min (T&& a, T&& b) { return a<b?a:b; }
+    Tps T max (T&& a, Ts&&... ts) { T b = max(ts...); return a>b?a:b; }
+    Tps T min (T&& a, Ts&&... ts) { T b = min(ts...); return a<b?a:b; }
+    Tps T chmax(T&& a, Ts&&... ts) { a = max(a, ts...); return a; }
+    Tps T chmin(T&& a, Ts&&... ts) { a = min(a, ts...); return a; }
+}
+namespace input { 
+    Tp void re(T&& x) { cin >> x; }
+    Tp2 void re(pair<T1,T2>& p) { re(p.ff); re(p.ss); }
+    Tp void re(vector<T>& a) { for(int i = 0; i < sz(a); i++) re(a[i]); }
+    Tp2 void rea(T1&& a, T2 t) { for(int i = 0; i < t; i++) re(a[i]); }
+    Tps2 void rea(T1&& a, T2 t, Ts&&... ts) { rea(a, t); rea(ts...); }
+    Tp2 void rea1(T1&& a, T2 t) { for(int i = 1; i <= t; i++) re(a[i]); }
+    Tps2 void rea1(T1&& a, T2 t, Ts&... ts) { rea1(a, t); rea1(ts...); }
+    Tps void re(T&& t, Ts&... ts) { re(t); re(ts...); }
+}
+namespace output {
+    void pr(int x) { cout << x; }
+    // void pr(num x) { cout << x; }
+    void pr(bool x) { cout << x; }
+    void pr(long long x) { cout << x; }
+    void pr(long long unsigned x) { cout << x; }
+    void pr(double x) { cout << x; }
+    void pr(char x) { cout << x; }
+    void pr(const string& x) { cout << x; }
+    void pr(const char* x) { cout << x; }
+    void pr(const char* x, size_t len) { cout << string(x, len); }
+    void ps() { cout << endl; }
+    void pn() { /*do nothing*/ }
+    void pw() { pr(" "); }
+    void pc() { pr("]"); ps(); }
+    Tp void pr(const T&);
+    bool parse(const char* t) { if(t == e) return true; return false;}
+    Tp bool parse(T&& t) { return false;}
+    Tp2 bool parsepair(const pair<T1,T2>& x) { return true; }
+    Tp bool parsepair(T&& t) { return false;}
+    Tp2 void psa(T1&& a, T2 t) { for(int i = 0; i < t; i++) pr(a[i]), pw(); ps(); }
+    Tp2 void psa2(T1&& a, T2 t1, T2 t2) { for(int i = 0; i < t1; i++) {for(int j = 0; j < t2; j++) pr(a[i][j]), pw(); ps();} }
+    Tp2 void pr(const pair<T1,T2>& x) { pr(x.ff); pw(); pr(x.ss);}
+    Tp void pr(const T& x) { bool fst = 1; bool op = 0; if (parsepair(*x.begin())) op = 1; for (const auto& a: x) {if(!fst) pw(); if(op) pr("{"); pr(a), fst = 0; if(op) pr("}"); } }
+    Tps void pr(const T& t, const Ts&... ts) { pr(t); pr(ts...); }
+    Tps void ps(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) {if (!parse(t)) pw(); } ps(ts...); }
+    Tp void pn(const T& t) { for (const auto& a: t) ps(a); }
+    Tps void pw(const T& t, const Ts&... ts) { pr(t); if (sizeof...(ts)) pw(); pw(ts...); }
+    Tps void pc(const T& t, const Ts&... ts) { bool op = 0; if (parsepair(t)) op = 1; if(op) pr("{"); pr(t); if(op) pr("}"); if (sizeof...(ts)) pr(", "); pc(ts...); }
+    namespace trace {
+        #define tr(x...) pr("[",#x,"] = ["), pc(x);
+        #define tra(x, y...) __f0(#x, x, y)
+        #define tran(x, n) __fn(n, #x, x) // TO DO~ variadic multidimensional
+        Tp2 void __f(const char* name, const T1& x, const T2& y){ pr("[",y,"] = "); ps(x); }
+        Tps2 void __f(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr("[",y,"]"); __f(open+1, x, rest...); }
+        Tps2 void __f0(const char* name, const T1& x, const T2& y, const Ts&... rest){ const char *open = strchr(name, '['); pr(name, size_t(open-name)); __f(name, x, y, rest...); }
+        Tp void __fn(int n, const char* name, const T& x) { for(int i = 0; i < n; i++) pr(name), __f(name, x[i], i); }
+    }
+}
+using namespace maxmin;
+using namespace input;
+using namespace output;
+using namespace output::trace;
 using ll = long long;
 using pii = pair<int, int>;
-constexpr int MOD = 1e9 + 7;
-constexpr int INF = 2e9;
-constexpr ll DESPACITO = 1e18;
-
-#ifdef LOCAL
-    constexpr bool loc = true;
-    #define tr(...) pr("(", __LINE__, ")[", #__VA_ARGS__, "]: ["), pc(__VA_ARGS__)
-    #define tra(arg, args...) ptrace(#arg, arg, args)
-#else
-    constexpr bool loc = false;
-    #define tr(...) "🍪"
-    #define tra(...) "🍪"
-#endif
-
-template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
-template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
-auto re = [](auto&&... args) { (cin >> ... >> args); };
-auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
-auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
-auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
-auto isLOC = [](auto A, string B = "") { return loc? A : B; };
-template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2> p) 
-{ os << isLOC("(") << p.first << isLOC(", ", " ") << p.second << isLOC(")"); return os; }
-#define Vout(Con) template<class T> ostream& operator<<(ostream& os, Con<T>& v) { bool fst = 1; os << isLOC("{"); \
-for(auto& x: v) { if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
-#define Vout2(Con) template<class T1, class T2> ostream& operator<<(ostream& os, Con<T1,T2>& v) { bool fst = 1; os << isLOC("{"); \
-for(auto& [x, y]: v) { if(!fst) os << isLOC(", ", " "); os << x << isLOC(": ", " ") << y; fst = 0; } os << isLOC("}"); return os; }
-Vout(vector) Vout(set) Vout(multiset) Vout2(map)
-#define Out(f, delim, lst) auto f = [](auto&& arg, auto&&... args) { cout << arg; ((cout << delim << args), ...); cout << lst; };
-Out(pr, "", "") Out(pw, ' ', ' ') Out(ps, ' ', endl) Out(pc, ", ", "]" << endl)
-auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
-auto ptrace = [](const char* name, auto&& A, auto&&... rest) {
-    const char* open = strchr(name, '['); cout.write(name, open-name);
-    ((cout << '[' << rest << ']'), ...); cout << " = " << A << endl;
-};
-
 const int N = 2e5 + 5;
-int who[N], pos[N];
+int pos[N];
 int32_t main() {
-    k_II
-    int i, Q, curl = 0, curr = 0;
+    IOS;
+    int i, Q, l = 0, r = 0;
     re(Q);
     while(Q--) {
-        char c; int id; re(c, id);
-        if(c == '?') {
-            int ans = INF;
-            if(who[id])
-                ans = min(curl-pos[id], pos[id]-1+curr);
-            else
-                ans = min(curr-pos[id], pos[id]-1+curl);
-            ps(ans);
-        }
-        else if(c == 'L') {
-            who[id] = 1;
-            pos[id] = ++curl;
-        }
-        else {
-            who[id] = 0;
-            pos[id] = ++curr;
-        }
+        char c; re(c, i);
+        if (c == 'L')
+            pos[i] = l++;
+        else if (c == 'R')
+            pos[i] = -(++r);
+        else
+            ps(min(pos[i]+r, l-pos[i]-1));
     }
-    
-    W
+    return 0;
 }
-
-/**
-    New Levels, new deri$s
-
-**/
+//is a bruteforce possible?
+//think greedier, make more assumptions
+//stuck for more than 5 minutes? move on
