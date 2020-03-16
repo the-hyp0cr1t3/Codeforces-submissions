cc686a6a8dda8190ae482b6ccb5d12d11decab2f
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-03-16 16:13:00 +0530

    Add solution for problem `F - Ehab's Last Theorem` Link: https://codeforces.com/contest/1325/problem/F Tags: constructive algorithms, dfs and similar, graphs, greedy, *2500 Ref: https://codeforces.com/contest/1325/submission/73399733

--- codeforces/1325/F.cpp
+++ codeforces/1325/F.cpp
@@ -2,9 +2,29 @@
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 using namespace std;
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define max3(x,y,z) max(x,max(y,z))
+#define min3(x,y,z) min(x,min(y,z))
+#define ff first
+#define ss second
+#define rev(a) reverse(a.begin(),a.end())
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define lb lower_bound
+#define ub upper_bound
 #define rsz resize
+#define ins insert
 #define pb emplace_back
 #define sz(x) (int)(x).size()
+#define MOD 1000000007  //1e9 + 7
+#define INF 2000000000 //2e9
+#define DESPACITO 1000000000000000000 //1e18
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+template<class T> T chmin(T &a, T b) { a = min(a, b); return a; }
+template<class T> T chmax(T &a, T b) { a = max(a, b); return a; }
+using pii = pair<int, int>;
+using ll = long long;
 const int N = 2e5 + 5;
 int n, m, k = 1, d[N], par[N];
 vector<int> g[N], indsets[N];
