c2a7305f2cd694736ca0640681bd9498b8e87d9c
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-06-11 03:12:00 +0530

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/83336629

--- codeforces/4/A.cpp
+++ codeforces/4/A.cpp
@@ -1,83 +1,23 @@
 /**
  *  the_hyp0cr1t3
- *  03.07.2020 04:04:42
+ *  11.06.2020 03:10:43
 **/
 #include <bits/stdc++.h>
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-// #define int long long
-#define endl "\n"
-#define ff first
-#define ss second
-#define rev(Aa) reverse(Aa.begin(),Aa.end())
-#define all(Aa) Aa.begin(),Aa.end()
-#define ins insert
-#define mp make_pair
-#define pb emplace_back
-#define sz(Xx) (int)(Xx).size()
-
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-const int MOD = 1e9 + 7;
-const int INF = 2e9;
-const ll DESPACITO = 1e18;
-
-namespace input {
-    template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
-    template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
-    auto re = [](auto&&... args) { (cin >> ... >> args); };
-    auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
-} using namespace input;
-
-namespace util {
-    template<class T> class y_combinator {
-        T f_;
-    public:
-        template<class U> explicit y_combinator(U&& f): f_(forward<U>(f)) {}
-        template<class ...Args> decltype(auto) operator()(Args&&... args) {
-            return f_(ref(*this), forward<Args>(args)...);
-        }
-    }; 
-    template<class T> decltype(auto) Y(T&& f) {
-        return y_combinator<decay_t<T>>(forward<T>(f));
-    }
-    auto chmax = [] (auto& A, auto&& B) { A = max(A, B); };
-    auto chmin = [] (auto& A, auto&& B) { A = min(A, B); };
-} using namespace util;
-
-namespace output {
-    char delim = ' ';
-    template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2>& p) { os << p.first << ' ' << p.second; return os; }
-    template<class T> ostream& operator<<(ostream& os, vector<T>& v) { if(v.size()) { os << v[0]; for(int i = 1; i < (int)v.size(); i++) os << delim << v[i]; } return os; }
-    auto pr = [](auto&& arg, auto&&... args) { cout << arg; ((cout << "" << args), ...); };
-    auto pw = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << ' '; };
-    auto ps = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << endl; };
-    auto pc = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ", " << args), ...); cout << ']' << endl; };
-    auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
-    auto pn = [](auto&& arg, auto&&... args) { delim = '\n'; cout << arg; ((cout << endl << args), ...); cout << endl; delim = ' '; };
-    auto ptrace = [](auto&& arg, auto&&... args) { cout << '[' << arg; ((cout << "][" << args), ...); cout << ']'; };
-    #define tr(args...) pr('[', #args, "] = ["), pc(args);
-    #define tra(arg, args...) __f(#arg, arg, args)
-    template<class arg, class ...args> void __f(const char* name, arg&& A, args&&... rest) { 
-        const char* open = strchr(name, '['); cout.write(name, open-name); 
-        ptrace(rest...); cout << " = " << A << endl;
-    }
-} using namespace output;
-
-const int N = 2e5 + 5;
 
 int32_t main() {
-    IOS;
-    int i, n;
-    re(n);
-    ps((n-2) and n%2 == 0? "YES" : "NO");
+    int n;
+    cin >> n;
+    cout << (n > 2 and n%2 == 0? "YES" : "NO");
     return 0;
 }
 
 /**
-    Salvatio@ wai#d witho&t reirieve
-    I'm o. a Nazor's eJNe anh it wuos mf feet
-    @s goom as life wihl ,vTr get
-    I eiry tEaw x could bive it up and go hote
+    Show yCuu Lards Rcd acYept
+    What youcvu been Cealt
+    Guardian or harfAinger Lf mhrrot?
+    &o#r a drinkr rahse a glass
+    And koast to he&lfh
+    hs thT Wruth ao@ hard for us to CwaOloy?
 
 **/
