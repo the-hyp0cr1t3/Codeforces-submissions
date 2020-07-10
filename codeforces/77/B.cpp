3b7526a61c21d446457c1f38cdee3633561a5e88
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-07-10 23:44:00 +0530

    Add solution for problem `B - Falling Anvils` Link: https://codeforces.com/contest/77/problem/B Tags: math, probabilities, *1800 Ref: https://codeforces.com/contest/77/submission/86452339

---
+++ codeforces/77/B.cpp
@@ -0,0 +1,86 @@
+/**
+ *  the_hyp0cr1t3
+ *  10.07.2020 23:30:16
+**/
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// #define int long long
+#define endl "\n"
+#define ff first
+#define ss second
+#define rev(Aa) reverse(Aa.begin(),Aa.end())
+#define all(Aa) Aa.begin(),Aa.end()
+#define ins insert
+#define mp make_pair
+#define pb emplace_back
+#define sz(Xx) (int)(Xx).size()
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+const ll DESPACITO = 1e18;
+
+template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
+auto re = [](auto&&... args) { (cin >> ... >> args); };
+auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
+auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
+auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
+char delim = ' ';
+template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2>& p) 
+{ os << p.first << ' ' << p.second; return os; }
+template<class T> ostream& operator<<(ostream& os, set<T>& v) { bool fst = 1; for(auto& x: v) 
+{ if(!fst) os << delim; os << x; fst = 0; } return os; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { bool fst = 1; for(auto& x: v) 
+{ if(!fst) os << delim; os << x; fst = 0; } return os; }
+template<class T1, class T2> ostream& operator<<(ostream& os, map<T1,T2>& v) { 
+bool fst = 1; for(auto& x: v) { if(!fst) os << delim; os << '{' << x << '}'; fst = 0; } return os; }
+auto pr = [](auto&& arg, auto&&... args) { cout << arg; ((cout << "" << args), ...); };
+auto pw = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << ' '; };
+auto ps = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << endl; };
+auto pc = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ", " << args), ...); cout << ']' << endl; };
+auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
+auto pn = [](auto&& arg, auto&&... args) { delim = '\n'; cout << arg; ((cout << endl << args), ...); cout << endl; delim = ' '; };
+auto ptrace = [](auto&& arg, auto&&... args) { cout << '[' << arg; ((cout << "][" << args), ...); cout << ']'; };
+#define tr(args...) pr('[', #args, "] = ["), pc(args);
+#define tra(arg, args...) __f(#arg, arg, args)
+template<class arg, class ...args> void __f(const char* name, arg&& A, args&&... rest) { 
+    const char* open = strchr(name, '['); cout.write(name, open-name); 
+    ptrace(rest...); cout << " = " << A << endl;
+}
+
+const int N = 2e5 + 5;
+
+int solve () {
+    int i, a, b;
+    re(a, b);
+    double ans;
+    if(!b)
+        ans = 1;
+    else if(!a)
+        ans = 0.5;
+    else if(a > 4*b)
+        ans = 1.0 - 1.0*b/a;
+    else
+        ans = 1.0*(8.0*b + a)/16/b;
+    cout << fixed << setprecision(9) << ans << endl;
+    return 0;
+}
+
+int32_t main() {
+    IOS;
+    int Q;
+    for(cin >> Q; Q; Q--)
+        solve();
+    return 0;
+}
+
+/**
+    The CNckroaxh King Xeus on his t#rone
+    With the @ddaz HVuch aHd a heaYt of z.one
+    kn empVqe bLLlD on guile aCd gheed
+    A bweedinT ground for thoOe whk Ne&d
+
+**/
