13b769c6cd004cf985e7e50eaa5b449ed9842207
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-07-11 16:04:00 +0530

    Add solution for problem `C - Powers Of Two` Link: https://codeforces.com/contest/1095/problem/C Tags: bitmasks, greedy, *1400 Ref: https://codeforces.com/contest/1095/submission/86494578

---
+++ codeforces/1095/C.cpp
@@ -0,0 +1,85 @@
+/**
+ *  the_hyp0cr1t3
+ *  11.07.2020 15:57:32
+**/
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// #define int long long
+#define endl "\n"
+#define ff first
+#define ss second
+#define rev(Aa) reverse(Aa.begin(),Aa.end())
+#define all(Aa) Aa.begin(),Aa.end()
+#define ins insert
+#define mp make_pair
+#define pb emplace_back
+#define sz(Xx) (int)(Xx).size()
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+const ll DESPACITO = 1e18;
+
+template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
+auto re = [](auto&&... args) { (cin >> ... >> args); };
+auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
+auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
+auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
+char delim = ' ';
+template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2>& p) 
+{ os << p.first << ' ' << p.second; return os; }
+template<class T> ostream& operator<<(ostream& os, set<T>& v) { bool fst = 1; for(auto& x: v) 
+{ if(!fst) os << delim; os << x; fst = 0; } return os; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { bool fst = 1; for(auto& x: v) 
+{ if(!fst) os << delim; os << x; fst = 0; } return os; }
+template<class T1, class T2> ostream& operator<<(ostream& os, map<T1,T2>& v) { 
+bool fst = 1; for(auto& x: v) { if(!fst) os << delim; os << '{' << x << '}'; fst = 0; } return os; }
+auto pr = [](auto&& arg, auto&&... args) { cout << arg; ((cout << "" << args), ...); };
+auto pw = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << ' '; };
+auto ps = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << endl; };
+auto pc = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ", " << args), ...); cout << ']' << endl; };
+auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
+auto pn = [](auto&& arg, auto&&... args) { delim = '\n'; cout << arg; ((cout << endl << args), ...); cout << endl; delim = ' '; };
+auto ptrace = [](auto&& arg, auto&&... args) { cout << '[' << arg; ((cout << "][" << args), ...); cout << ']'; };
+#define tr(args...) pr('[', #args, "] = ["), pc(args);
+#define tra(arg, args...) __f(#arg, arg, args)
+template<class arg, class ...args> void __f(const char* name, arg&& A, args&&... rest) { 
+    const char* open = strchr(name, '['); cout.write(name, open-name); 
+    ptrace(rest...); cout << " = " << A << endl;
+}
+
+const int N = 2e5 + 5;
+
+int32_t main() {
+    IOS;
+    int i, n, k, cnt = 0;
+    re(n, k);
+    if(k > n) return ps("NO"), 0;
+    bitset<33> b(n);
+    vector<int> ans(33);
+    for(i = 0; i < 33; i++) {
+        if(b[i]) ans[i] = 1, cnt++;
+    }
+    if(k < cnt) return ps("NO"), 0;
+    k -= cnt;
+    for(i = 32; i and k; i--) {
+        if(!ans[i]) continue;
+        while(k and ans[i])
+            ans[i-1] += 2, k--, ans[i]--;
+    }
+    if(k) return ps("NO"), 0;
+    ps("YES");
+    for(i = 0; i < 33; i++) {
+        for(int j = 0; j < ans[i]; j++) 
+            pw(1<<i);
+    }
+    return 0;
+}
+
+/**
+    Lxok but dLn't Ioucc
+
+**/
