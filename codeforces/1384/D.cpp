d17b4df663796cb8a886dcac5cd349b552a77668
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-07-25 04:06:00 +0530

    Add solution for problem `D - GameGame` Link: https://codeforces.com/contest/1384/problem/D Tags: bitmasks, constructive algorithms, games, math, *1900 Ref: https://codeforces.com/contest/1384/submission/87945485

---
+++ codeforces/1384/D.cpp
@@ -0,0 +1,96 @@
+/**
+ *  the_hyp0cr1t3
+ *  25.07.2020 04:00:46
+**/
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// #define int long long
+#define endl "\n"
+#define ff first
+#define ss second
+#define rev(Aa) reverse(Aa.begin(),Aa.end())
+#define all(Aa) Aa.begin(),Aa.end()
+#define ins insert
+#define mp make_pair
+#define pb emplace_back
+#define sz(Xx) (int)(Xx).size()
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+const ll DESPACITO = 1e18;
+
+#ifdef LOCAL
+    bool loc = true;
+#else
+    bool loc = false;
+#endif
+
+template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
+auto re = [](auto&&... args) { (cin >> ... >> args); };
+auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
+auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
+auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
+auto isLOC = [](auto A, string B = "") { return loc? A : B; };
+template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2> p) 
+{ os << isLOC("(") << p.first << isLOC(", ", " ") << p.second << isLOC(")"); return os; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { bool fst = 1; os << isLOC("{"); for(auto& x: v) 
+{ if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
+template<class T> ostream& operator<<(ostream& os, set<T>& v) { bool fst = 1; os << isLOC("{"); for(auto& x: v) 
+{ if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
+template<class T1, class T2> ostream& operator<<(ostream& os, map<T1,T2>& v) { bool fst = 1; os << isLOC("{"); for(auto& [x, y]: v)
+{ if(!fst) os << isLOC(", ", " "); os << x << isLOC(": ", " ") << y; fst = 0; } os << isLOC("}"); return os; }
+auto pr = [](auto&& arg, auto&&... args) { cout << arg; ((cout << "" << args), ...); };
+auto pw = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << ' '; };
+auto ps = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << endl; };
+auto pc = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ", " << args), ...); cout << ']' << endl; };
+auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
+auto ptrace = [](auto&& arg, auto&&... args) { cout << '[' << arg; ((cout << "][" << args), ...); cout << ']'; };
+#define tr(...) pr("(", __LINE__, ")[", #__VA_ARGS__, "]: ["), pc(__VA_ARGS__);
+#define tra(arg, args...) __f(#arg, arg, args)
+template<class arg, class ...args> void __f(const char* name, arg&& A, args&&... rest) { 
+    const char* open = strchr(name, '['); cout.write(name, open-name);
+     ptrace(rest...); cout << " = " << A << endl;
+}
+
+const int N = 2e5 + 5;
+int a[N];
+int solve () {
+    int i, j, n;
+    re(n); rea(a, n);
+    vector<int> cnt(31);
+    for(i = 0; i < n; i++)
+        for(j = 0; j < 31; j++) 
+            cnt[j] += !!(a[i]&(1<<j));
+    for(i = 30; ~i; i--) {
+        if(cnt[i]&1) {
+            if((n-cnt[i])&1 or (((cnt[i]-1)/2)&1) == 0)
+                return ps("WIN"), 0;
+            else
+                return ps("LOSE"), 0;
+        }
+    }
+    ps("DRAW");
+    return 0;
+}
+
+int32_t main() {
+    IOS;
+    int Q;
+    for(cin >> Q; Q; Q--)
+        solve();
+    return 0;
+}
+
+/**
+    rIMw your cacds and accedt
+    What z$.'v@ been dealt
+    GjardiaI or h@rbriwger of sov&pwe
+    ooue a Dronk, raise a glasG
+    And DAast Mo healt^
+    I@ J$e aruth eoo harn jor us ty swRllow?
+
+**/
