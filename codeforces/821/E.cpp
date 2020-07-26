afb612a9e884f3540b4fb381ea587478a2506746
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-07-26 23:43:00 +0530

    Add solution for problem `E - Okabe and El Psy Kongroo` Link: https://codeforces.com/contest/821/problem/E Tags: dp, matrices, *2100 Ref: https://codeforces.com/contest/821/submission/88078573

---
+++ codeforces/821/E.cpp
@@ -0,0 +1,198 @@
+/**
+ *  the_hyp0cr1t3
+ *  26.07.2020 23:36:18
+**/
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// #define int long long
+#define endl "\n"
+#define ff first
+#define ss second
+#define rev(Aa) reverse(Aa.begin(),Aa.end())
+#define all(Aa) Aa.begin(),Aa.end()
+#define ins insert
+#define mp make_pair
+#define pb emplace_back
+#define sz(Xx) (int)(Xx).size()
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+const ll DESPACITO = 1e18;
+
+#ifdef LOCAL
+    bool loc = true;
+#else
+    bool loc = false;
+#endif
+
+template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
+auto re = [](auto&&... args) { (cin >> ... >> args); };
+auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
+auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
+auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
+auto isLOC = [](auto A, string B = "") { return loc? A : B; };
+template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2> p) 
+{ os << isLOC("(") << p.first << isLOC(", ", " ") << p.second << isLOC(")"); return os; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { bool fst = 1; os << isLOC("{"); for(auto& x: v) 
+{ if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
+template<class T> ostream& operator<<(ostream& os, set<T>& v) { bool fst = 1; os << isLOC("{"); for(auto& x: v) 
+{ if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
+template<class T1, class T2> ostream& operator<<(ostream& os, map<T1,T2>& v) { bool fst = 1; os << isLOC("{"); for(auto& [x, y]: v)
+{ if(!fst) os << isLOC(", ", " "); os << x << isLOC(": ", " ") << y; fst = 0; } os << isLOC("}"); return os; }
+auto pr = [](auto&& arg, auto&&... args) { cout << arg; ((cout << "" << args), ...); };
+auto pw = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << ' '; };
+auto ps = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << endl; };
+auto pc = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ", " << args), ...); cout << ']' << endl; };
+auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
+auto ptrace = [](auto&& arg, auto&&... args) { cout << '[' << arg; ((cout << "][" << args), ...); cout << ']'; };
+#define tr(...) pr("(", __LINE__, ")[", #__VA_ARGS__, "]: ["), pc(__VA_ARGS__);
+#define tra(arg, args...) __f(#arg, arg, args)
+template<class arg, class ...args> void __f(const char* name, arg&& A, args&&... rest) { 
+    const char* open = strchr(name, '['); cout.write(name, open-name);
+     ptrace(rest...); cout << " = " << A << endl;
+}
+struct num {
+    typedef decay<decltype(MOD)>::type T;
+    T v; 
+    static T inv(T a, T m) { a %= m; return a == 1 ? 1 : (T)(m - ll(inv(m, a)) * ll(m) / a); } 
+    num() : v(0) {}
+    num(ll v_) {
+        v = (-MOD < v_ && v_ < MOD) ? T(v_) : T(v_ % MOD);
+        if (v < 0) v += MOD;
+    }
+    explicit operator T() const { return v; }
+    friend std::ostream& operator << (std::ostream& out, const num& n) { return out << T(n); }
+    friend std::istream& operator >> (std::istream& in, num& n) { ll v_; in >> v_; n = num(v_); return in; } 
+    friend bool operator == (const num& a, const num& b) { return a.v == b.v; }
+    friend bool operator != (const num& a, const num& b) { return a.v != b.v; }
+    num inv() const { num res; res.v = inv(v, MOD); return res; }
+    num operator- () const { num res; res.v = v ? MOD-v : 0; return res; }
+    num operator+ () const { return num(*this); }
+    num& operator ++ () { v ++; if (v == MOD) v = 0; return *this; }
+    num& operator -- () { if (v == 0) v = MOD; v --; return *this; }
+    num& operator += (const num& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    num& operator -= (const num& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    num& operator *= (const num& o) { v = T(ll(v) * ll(o.v) % MOD); return *this; }
+    num& operator /= (const num& o) { return *this *= o.inv(); }
+    friend num operator ++ (num& a, int) { num r = a; ++a; return r; }
+    friend num operator -- (num& a, int) { num r = a; --a; return r; }
+    friend num operator + (const num& a, const num& b) { return num(a) += b; }
+    friend num operator - (const num& a, const num& b) { return num(a) -= b; }
+    friend num operator * (const num& a, const num& b) { return num(a) *= b; }
+    friend num operator / (const num& a, const num& b) { return num(a) /= b; }
+    friend bool operator < (const num& a, const num& b) { return a.v < b.v; }
+    friend bool operator <= (const num& a, const num& b) { return a.v <= b.v; }
+    friend bool operator > (const num& a, const num& b) { return a.v > b.v; }
+    friend bool operator >= (const num& a, const num& b) { return a.v >= b.v; }
+};
+
+template<typename T>
+struct Matrix {
+    int N, M;
+    vector<vector<T>> a;
+    Matrix(int n, int m): N(n), M(m) {
+        a = vector<vector<T>>(N, vector<T>(M));
+    }
+    explicit Matrix(int n, int m, T x): N(n), M(m) {
+        a = vector<vector<T>>(N, vector<T>(M, 0));
+        for(int i = 0; i < N and i < M; i++) a[i][i] = x;
+    }
+    Matrix(initializer_list<vector<T>> x) {
+        N = x.size(); M = x.begin()->size(); a.resize(N);
+        for(int i = 0; i < sz(x); i++)
+            a[i] = *(x.begin() + i), assert(sz(a[i]) == M);
+    }
+    friend ostream& operator<<(std::ostream& out, const Matrix& x) {
+        for(int i = 0; i < x.N; i++) 
+            for(int j = 0; j < x.M; j++) 
+                out << x.a[i][j] << " \n"[j == x.M-1];
+        return out;
+    }
+    vector<T>& operator[](int x) { 
+        return a[x]; 
+    }
+    const vector<T>& operator[](int x) const { 
+        return a[x]; 
+    }
+    Matrix& operator+=(const Matrix& x) {
+        assert(N == x.N and M == x.M);
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < M; j++)
+                a[i][j] += x[i][j];
+        return *this;
+    }
+    Matrix operator+(const Matrix& x) {
+        Matrix ans = *this;
+        return ans += x;
+    }
+    Matrix& operator-=(const Matrix& x) {
+        assert(N == x.N and M == x.M);
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < M; j++)
+                a[i][j] -= x[i][j];
+        return *this;
+    }
+    Matrix operator-(const Matrix& x) {
+        Matrix ans = *this;
+        return ans -= x;
+    }
+    Matrix operator*(const Matrix& x) {
+        Matrix<T> ans(N, x.M);
+        for(int i = 0; i < N; i++)
+            for(int k = 0; k < x.M; k++)
+                for(int j = 0; j < M; j++)
+                    ans[i][k] += a[i][j] * x[j][k];
+        return ans;
+    }
+    Matrix& operator*=(T x) {
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < M; j++)
+                a[i][j] *= x;
+        return *this;
+    }
+    Matrix operator*(T x) {
+        Matrix ans = *this;
+        return ans *= x;
+    }
+    template<typename K>
+    Matrix operator^(K x) {
+        assert(x >= 0);
+        Matrix res{N, M, 1}, A = *this; while(x) {
+            if(x & 1) res = res * A;
+            x >>= 1; A = A * A;
+        } return res;
+    }
+};
+
+const int N = 2e5 + 5;
+
+int32_t main() {
+    IOS;
+    int i, j, n; ll k;
+    re(n, k);
+    Matrix<num> B(16, 1);
+    B[0][0] = 1;
+    for(i = 0; i < n; i++) {
+        ll a, b; int c;
+        re(a, b, c);
+        b = min(b, k);
+        Matrix<num> F(16, 16);
+        for(j = 0; j <= c; j++) {
+            if(j-1 >= 0) F[j][j-1] = 1;
+            F[j][j] = 1;
+            if(j+1 <= c) F[j][j+1] = 1;
+        }
+        B = (F ^ (b-a)) * B;
+    }
+    ps(B[0][0]);
+    return 0;
+}
+
+/**
+    Look but donj. tovcd
+
+**/
