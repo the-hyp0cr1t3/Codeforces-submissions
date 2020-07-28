22f461c7541a249b68b1d92feae7fde49cf3254d
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-07-28 21:25:00 +0530

    Add solution for problem `C - Fools and Roads` Link: https://codeforces.com/contest/191/problem/C Tags: data structures, dfs and similar, trees, *1900 Ref: https://codeforces.com/contest/191/submission/88219837

--- codeforces/191/C.cpp
+++ codeforces/191/C.cpp
@@ -67,126 +67,13 @@ public:
 template<class T> Y(T) -> Y<T>;
 
 const int N = 1e5 + 5;
-int n, dp[N], ans[N];
+const int LG = 20;
+int d[N], anc[LG][N], dp[N], ans[N];
 vector<pii> g[N];
-int block_size, block_cnt;
-vector<int> first_visit;
-vector<int> euler_tour;
-vector<int> height;
-vector<int> log_2;
-vector<vector<int>> st;
-vector<vector<vector<int>>> blocks;
-vector<int> block_mask;
-
-void dfs(int v, int p, int h) {
-    first_visit[v] = euler_tour.size();
-    euler_tour.push_back(v);
-    height[v] = h;
-
-    for (auto [u, j] : g[v]) {
-        if (u == p)
-            continue;
-        dfs(u, v, h + 1);
-        euler_tour.push_back(v);
-    }
-}
-
-int min_by_h(int i, int j) {
-    return height[euler_tour[i]] < height[euler_tour[j]] ? i : j;
-}
-
-void precompute_lca(int root) {
-    // get euler tour & indices of first occurences
-    first_visit.assign(n, -1);
-    height.assign(n, 0);
-    euler_tour.reserve(2 * n);
-    dfs(root, -1, 0);
-
-    // precompute all log values
-    int m = euler_tour.size();
-    log_2.reserve(m + 1);
-    log_2.push_back(-1);
-    for (int i = 1; i <= m; i++)
-        log_2.push_back(log_2[i / 2] + 1);
-
-    block_size = max(1, log_2[m] / 2);
-    block_cnt = (m + block_size - 1) / block_size;
-
-    // precompute minimum of each block and build sparse table
-    st.assign(block_cnt, vector<int>(log_2[block_cnt] + 1));
-    for (int i = 0, j = 0, b = 0; i < m; i++, j++) {
-        if (j == block_size)
-            j = 0, b++;
-        if (j == 0 || min_by_h(i, st[b][0]) == i)
-            st[b][0] = i;
-    }
-    for (int l = 1; l <= log_2[block_cnt]; l++) {
-        for (int i = 0; i < block_cnt; i++) {
-            int ni = i + (1 << (l - 1));
-            if (ni >= block_cnt)
-                st[i][l] = st[i][l-1];
-            else
-                st[i][l] = min_by_h(st[i][l-1], st[ni][l-1]);
-        }
-    }
-
-    // precompute mask for each block
-    block_mask.assign(block_cnt, 0);
-    for (int i = 0, j = 0, b = 0; i < m; i++, j++) {
-        if (j == block_size)
-            j = 0, b++;
-        if (j > 0 && (i >= m || min_by_h(i - 1, i) == i - 1))
-            block_mask[b] += 1 << (j - 1);
-    }
-
-    // precompute RMQ for each unique block
-    int possibilities = 1 << (block_size - 1);
-    blocks.resize(possibilities);
-    for (int b = 0; b < block_cnt; b++) {
-        int mask = block_mask[b];
-        if (!blocks[mask].empty())
-            continue;
-        blocks[mask].assign(block_size, vector<int>(block_size));
-        for (int l = 0; l < block_size; l++) {
-            blocks[mask][l][l] = l;
-            for (int r = l + 1; r < block_size; r++) {
-                blocks[mask][l][r] = blocks[mask][l][r - 1];
-                if (b * block_size + r < m)
-                    blocks[mask][l][r] = min_by_h(b * block_size + blocks[mask][l][r], 
-                            b * block_size + r) - b * block_size;
-            }
-        }
-    }
-}
-
-int lca_in_block(int b, int l, int r) {
-    return blocks[block_mask[b]][l][r] + b * block_size;
-}
-
-int LCA(int v, int u) {
-    int l = first_visit[v];
-    int r = first_visit[u];
-    if (l > r)
-        swap(l, r);
-    int bl = l / block_size;
-    int br = r / block_size;
-    if (bl == br)
-        return euler_tour[lca_in_block(bl, l % block_size, r % block_size)];
-    int ans1 = lca_in_block(bl, l % block_size, block_size - 1);
-    int ans2 = lca_in_block(br, 0, r % block_size);
-    int ans = min_by_h(ans1, ans2);
-    if (bl + 1 < br) {
-        int l = log_2[br - bl - 1];
-        int ans3 = st[bl+1][l];
-        int ans4 = st[br - (1 << l)][l];
-        ans = min_by_h(ans, min_by_h(ans3, ans4));
-    }
-    return euler_tour[ans];
-}
 
 int32_t main() {
     Kuii
-    int i;
+    int i, n;
     re(n);
 
     for(i = 0; i < n-1; i++) {
@@ -195,10 +82,35 @@ int32_t main() {
         u--, v--;
         g[u].pb(v, i);
         g[v].pb(u, i);
-    }   
+    }
     
-    precompute_lca(0);
+    auto dfs1 = Y([&](auto self, int v, int p) -> void {
+        anc[0][v] = p;
+        for(int k = 1; k < LG; k++)
+            anc[k][v] = ~anc[k-1][v]? anc[k-1][anc[k-1][v]] : -1;
+        for(auto& [x, j]: g[v]) {
+            if(x == p) continue;
+            d[x] = d[v]+1;
+            self(x, v);
+        }
+    });
 
+    dfs1(0, -1);
+    
+    auto LCA = [&] (int u, int v) {
+        if(d[u] < d[v])
+            swap(u, v);
+        for(int z = LG-1; ~z; --z) 
+            if(d[u]-(1<<z) >= d[v])
+                u = anc[z][u];
+        if(u == v)
+            return u;
+        for(int z = LG-1; ~z; --z)
+            if(anc[z][u]^anc[z][v])
+                u = anc[z][u], v = anc[z][v];
+        return anc[0][u];
+    };
+    
     int Q; re(Q);
     while(Q--) {
         int u, v; re(u, v); u--; v--;
