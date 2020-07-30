459c10e8d7953319218321abb7ffb94c7212790d
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-07-30 20:14:00 +0530

    Add solution for problem `A - Captain Flint and Crew Recruitment` Link: https://codeforces.com/contest/1388/problem/A Tags: brute force, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1388/submission/88456120

---
+++ codeforces/1388/A.cpp
@@ -0,0 +1,95 @@
+/**
+ *  the_hyp0cr1t3
+ *  30.07.2020 20:06:43
+**/
+#include <bits/stdc++.h>
+// #define int long long
+#define k_II ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define DubYu return 0;
+#define endl "\n"
+#define ff first
+#define ss second
+#define rev(Aa) reverse(Aa.begin(),Aa.end())
+#define all(Aa) Aa.begin(),Aa.end()
+#define ins insert
+#define mp make_pair
+#define pb emplace_back
+#define sz(Xx) (int)(Xx).size()
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int MOD = 1e9 + 7;
+const int INF = 2e9;
+const ll DESPACITO = 1e18;
+
+#ifdef LOCAL
+    bool loc = true;
+#else
+    bool loc = false;
+#endif
+
+template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) { is >> p.first >> p.second; return is; }
+template<class T> istream& operator>>(istream& is, vector<T>& v) { for(auto& x: v) is >> x; return is; }
+auto re = [](auto&&... args) { (cin >> ... >> args); };
+auto rea = [](auto&& A, auto sz_) { for(int i = 0; i < sz_; i++) cin >> A[i]; };
+auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
+auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
+auto isLOC = [](auto A, string B = "") { return loc? A : B; };
+template<class T1, class T2> ostream& operator<<(ostream& os, pair<T1,T2> p) 
+{ os << isLOC("(") << p.first << isLOC(", ", " ") << p.second << isLOC(")"); return os; }
+template<class T> ostream& operator<<(ostream& os, vector<T>& v) { bool fst = 1; os << isLOC("{"); for(auto& x: v) 
+{ if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
+template<class T> ostream& operator<<(ostream& os, set<T>& v) { bool fst = 1; os << isLOC("{"); for(auto& x: v) 
+{ if(!fst) os << isLOC(", ", " "); os << x; fst = 0; } os << isLOC("}"); return os; }
+template<class T1, class T2> ostream& operator<<(ostream& os, map<T1,T2>& v) { bool fst = 1; os << isLOC("{"); for(auto& [x, y]: v)
+{ if(!fst) os << isLOC(", ", " "); os << x << isLOC(": ", " ") << y; fst = 0; } os << isLOC("}"); return os; }
+auto pr = [](auto&& arg, auto&&... args) { cout << arg; ((cout << "" << args), ...); };
+auto pw = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << ' '; };
+auto ps = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ' ' << args), ...); cout << endl; };
+auto pc = [](auto&& arg, auto&&... args) { cout << arg; ((cout << ", " << args), ...); cout << ']' << endl; };
+auto pnv = [](auto&& arg) { for(auto& v: arg) { cout << v; cout << endl; } };
+auto ptrace = [](auto&& arg, auto&&... args) { cout << '[' << arg; ((cout << "][" << args), ...); cout << ']'; };
+#define tr(...) pr("(", __LINE__, ")[", #__VA_ARGS__, "]: ["), pc(__VA_ARGS__);
+#define tra(arg, args...) __f(#arg, arg, args)
+template<class arg, class ...args> void __f(const char* name, arg&& A, args&&... rest) { 
+    const char* open = strchr(name, '['); cout.write(name, open-name);
+     ptrace(rest...); cout << " = " << A << endl;
+}
+
+const int N = 2e5 + 5;
+
+int solve () {
+    int i, n;
+    re(n);
+    if(n >= 31) {
+        ps("Yes");
+        if(n == 36)
+            ps(6, 10, 15, 5);
+        else if(n == 40)
+            ps(6, 10, 15, 9);
+        else if(n == 44)
+            ps(6, 7, 10, 21);
+        else
+            ps(6, 10, 14, n-30);
+    }
+    else
+        ps("No");
+    return 0;
+}
+
+int32_t main() {
+    k_II
+    int Q;
+    for(cin >> Q; Q; Q--)
+        solve();
+    DubYu
+}
+
+/**
+    axen Tp nUme becomes @omxrrow's news
+    fn ton of bKe @ountain I c#Unot be msved
+    i scale Che summit like an unMtK&pI&le force
+    Lith zrKn wings I Mijl pZoPe Cy wocth tM you all
+
+**/
