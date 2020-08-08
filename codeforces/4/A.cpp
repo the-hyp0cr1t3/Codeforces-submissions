4e78fc805b2b5351963b5707b302d93777dd1353
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-08-09 02:35:00 +0530

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/89363179

--- codeforces/4/A.cpp
+++ codeforces/4/A.cpp
@@ -1,29 +1,82 @@
 /**
  🍪 the_hyp0cr1t3
- 🍪 17.08.2020 02:02:59
+ 🍪 09.08.2020 02:34:23
 **/
-#ifdef W
-#include "k_II.h"
-#else
 #include <bits/stdc++.h>
-using namespace std;
-#endif
 // #define int long long
-#define endl '\n'
-#define sz(x) int((x).size())
-#define all(x) x.begin(), x.end()
-auto chmax = [](auto& A, auto&& B) { return B > A? A = B, true : false; };
-auto chmin = [](auto& A, auto&& B) { return B < A? A = B, true : false; };
+#define k_II cin.tie(nullptr)->sync_with_stdio(false);
+#define W return 0;
+#define endl "\n"
+#define ff first
+#define ss second
+#define rev(Aa) reverse(Aa.begin(),Aa.end())
+#define all(Aa) Aa.begin(),Aa.end()
+#define ins insert
+#define mp make_pair
+#define pb emplace_back
+#define sz(Xx) (int)(Xx).size()
 
+using namespace std;
 using ll = long long;
-const ll DESPACITO = 2e18;
-const int INF = 2e9, MOD = 1e9+7;
-const int N = 2e5+5;
+using pii = pair<int, int>;
+constexpr int MOD = 1e9 + 7;
+constexpr int INF = 2e9;
+constexpr ll DESPACITO = 1e18;
+
+#ifdef LOCAL
+    constexpr bool loc = true;
+    #define tr(...) cout << "(" << __LINE__ << ")["#__VA_ARGS__"]: [", pr(__VA_ARGS__), cout << "]"
+    #define tra(arg, args...) ptrace(#arg, arg, args)
+#else
+    constexpr bool loc = false;
+    #define tr(...) "🍪"
+    #define tra(...) "🍪"
+#endif
+
+#define SFINAE(x, ...)  template <class, class = void> struct x : false_type {}; template <class T> struct x<T, void_t<__VA_ARGS__>> : true_type {};
+SFINAE(DefaultIO, decltype(cout << declval<T&>())) SFINAE(IsTuple, typename tuple_size<T>::type) SFINAE(Iterable, decltype(begin(declval<T>())))
+auto chmax = [](auto& A, auto&& B) { A = max(A, B); };
+auto chmin = [](auto& A, auto&& B) { A = min(A, B); };
+auto isLOC = [](auto A, string B = "") { return loc? A : B; };
+template<class T> void re(T& t) {
+    if constexpr(DefaultIO<T>::value) cin >> t;
+    else if constexpr(Iterable<T>::value) for(auto& x: t) re(x);
+    else if constexpr(IsTuple<T>::value) apply([&](auto&... args) { (re(args), ...); }, t);
+    else static_assert(DefaultIO<T>::value, "No matching type for read");
+}
+template<typename T> enable_if_t<is_pointer<T>::value, void> re(T beg, T end) { for(T it = beg; it != end; ++it) cin >> *it; }
+template<class... Ts> void re(Ts&... ts) { (re(ts), ...); }
+template<class T> constexpr void space(T) { if constexpr(Iterable<T>::value) cout << isLOC(",") << endl; else cout << isLOC(", "," "); }
+template<class T> void pr(const T& t) {
+    if constexpr(DefaultIO<T>::value) cout << t;
+    else if constexpr(Iterable<T>::value) { cout << isLOC("{"); bool fst = 1; 
+        for(auto&& x: t) { if(fst) fst = 0; else space(x); pr(x); } cout << isLOC("}"); }
+    else if constexpr(IsTuple<T>::value) { cout << isLOC("("); apply([&](const auto&... args) 
+        { bool fst = 1; ((fst ? (fst = 0, pr(args)) : (space(args), pr(args))), ...); }, t); cout << isLOC(")"); }
+    else static_assert(DefaultIO<T>::value, "No matching type for print");
+}
+template<typename T> enable_if_t<is_pointer<T>::value, void> pr(T beg, T end) 
+{ cout << isLOC("{"); for(T it = beg; it != end; ++it) { if(it != beg) space(*it); pr(*it); } cout << isLOC("}"); }
+template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { pr(t), ((space(ts), pr(ts)), ...); }
+template<class... Ts> void ps(Ts&&... ts) { pr(ts...); cout << endl; }
+template<class... Ts> void pw(Ts&&... ts) { pr(ts...); cout << ' '; }
+template<class T, class... Ts> void ptrace(const char* name, T&& A, Ts&&... rest) {
+    const char* open = strchr(name, '['); cout.write(name, open-name);
+    ((cout << '[' << rest << ']'), ...); cout << " = "; ps(A);
+}
+
+const int N = 2e5 + 5;
 
 int32_t main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
+    k_II
     int i, n;
-    cin >> n;
-    cout << (n&1 or n == 2? "NO" : "YES");
-    return 0;
+    re(n);
+    ps(n&1^1 and n^2? "YES" : "NO");
+
+    W
 }
+
+/**
+    New levelR, Pew devil,
+
+**/
