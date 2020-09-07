5217236476e91edda955e1a002c5ed7f311c4e7b
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-09-07 09:15:00 +0530

    Add solution for problem `A - Permutation Forgery` Link: https://codeforces.com/contest/1405/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1405/submission/92101381

---
+++ codeforces/1405/A.cpp
@@ -0,0 +1,42 @@
+/**
+ 🍪 thew6rst
+ 🍪 07.09.2020 09:14:59
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#define aylmao cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+#define endl '\n'
+#define pb emplace_back
+// #define int int64_t
+#define sz(x) (int)((x).size())
+#define all(x) x.begin(), x.end()
+#define Luv(...) [&](auto&& u, auto&& v) { return __VA_ARGS__; }
+auto chmax = [](auto& A, auto&& B) { return B > A? A = B, true : false; };
+auto chmin = [](auto& A, auto&& B) { return B < A? A = B, true : false; };
+
+using ll = int64_t;
+using pii = pair<int,int>;
+const ll DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+
+const int N = 2e5 + 5;
+int a[N];
+int the_real_main() {
+    int i, n;
+    cin >> n;
+    for(i = 0; i < n; i++) 
+        cin >> a[i];
+    for(i = n-1; ~i; i--) 
+        cout << a[i] << " \n"[i == 0];
+    return 0;
+}
+
+int32_t main() {
+    aylmao
+    int Q; for(cin >> Q; Q; Q--) the_real_main();
+    return 0;
+}
