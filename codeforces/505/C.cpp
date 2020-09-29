0754b1d06089a1745322d674b7936eb68398863f
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-09-29 17:09:00 +0530

    Add solution for problem `C - Mr. Kitayuta, the Treasure Hunter` Link: https://codeforces.com/contest/505/problem/C Tags: dfs and similar, dp, two pointers, *1900 Ref: https://codeforces.com/contest/505/submission/94204382

--- codeforces/505/C.cpp
+++ codeforces/505/C.cpp
@@ -13,6 +13,19 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 auto chmax = [](auto& A, auto&& B) { return B > A? A = B, true : false; };
 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
 const int64_t DESPACITO = 2e18;
 const int INF = 2e9, MOD = 1e9+7;
 const int N = 30001;
