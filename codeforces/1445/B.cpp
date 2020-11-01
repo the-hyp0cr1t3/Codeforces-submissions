bfbcd56c01147baac7e52f6e4fadde206380c3f3
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-11-01 22:28:00 +0530

    Add solution for problem `B - Elimination` Link: https://codeforces.com/contest/1445/problem/B Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1445/submission/97374050

---
+++ codeforces/1445/B.cpp
@@ -0,0 +1,29 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 01.11.2020 22:27:27
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+const int N = 2e5 + 5;
+
+int the_real_main() {
+    int i, a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << max(a+b, c+d) << '\n';
+    return 0;
+}
+
+int32_t main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int Q; for(cin >> Q; Q; Q--) the_real_main();
+}
