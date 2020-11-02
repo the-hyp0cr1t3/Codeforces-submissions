6ba062e1692d36d1f7da88cf2d2145d63f25df02
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-11-02 15:52:00 +0530

    Add solution for problem `E - Children and Candies` Link: https://atcoder.jp/contests/arc059/tasks/arc059_c Tags: AtCoder, *800 Ref: https://atcoder.jp/contests/arc059/submissions/17839398

---
+++ atcoder/arc059/E.cpp
@@ -0,0 +1,102 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 02.11.2020 15:34:55
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+
+template<const int& MOD>
+struct Mint {
+    using T = decay<decltype(MOD)>::type; T v;
+    Mint(int64_t v = 0) { if(v < 0) v = v % MOD + MOD; if(v >= MOD) v %= MOD; this->v = T(v); }
+    Mint(uint64_t v) { if (v >= MOD) v %= MOD; this->v = T(v); }
+    Mint(int v): Mint(int64_t(v)) {}
+    Mint(unsigned v): Mint(uint64_t(v)) {}
+    explicit operator int() const { return v; }
+    explicit operator int64_t() const { return v; }
+    explicit operator uint64_t() const { return v; }
+    friend istream& operator>>(istream& in, Mint& m) { int64_t v_; in >> v_; m = Mint(v_); return in; } 
+    friend ostream& operator<<(ostream& os, const Mint& m) { return os << m.v; }
+
+    static T inv(T a, T m) {
+        T g = m, x = 0, y = 1;
+        while(a != 0) {
+            T q = g / a;
+            g %= a; swap(g, a);
+            x -= q * y; swap(x, y);
+        } return x < 0? x + m : x;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif // x must be less than 2^32 * m
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x), quot, rem;
+        asm("divl %4\n" : "=a" (quot), "=d" (rem) : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    Mint inv() const { return Mint(inv(v, MOD)); }
+    Mint operator-() const { return Mint(v? MOD-v : 0); }
+    Mint& operator++() { v++; if(v == MOD) v = 0; return *this; }
+    Mint& operator--() { if(v == 0) v = MOD; v--; return *this; }
+    Mint operator++(int) { Mint a = *this; ++*this; return a; }
+    Mint operator--(int) { Mint a = *this; --*this; return a; }
+    Mint& operator+=(const Mint& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    Mint& operator-=(const Mint& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    Mint& operator*=(const Mint& o) { v = fast_mod(uint64_t(v) * o.v); return *this; }
+    Mint& operator/=(const Mint& o) { return *this *= o.inv(); }
+    friend Mint operator+(const Mint& a, const Mint& b) { return Mint(a) += b; }
+    friend Mint operator-(const Mint& a, const Mint& b) { return Mint(a) -= b; }
+    friend Mint operator*(const Mint& a, const Mint& b) { return Mint(a) *= b; }
+    friend Mint operator/(const Mint& a, const Mint& b) { return Mint(a) /= b; }
+    friend bool operator==(const Mint& a, const Mint& b) { return a.v == b.v; }
+    friend bool operator!=(const Mint& a, const Mint& b) { return a.v != b.v; }
+    friend bool operator<(const Mint& a, const Mint& b) { return a.v < b.v; }
+    friend bool operator>(const Mint& a, const Mint& b) { return a.v > b.v; }
+    friend bool operator<=(const Mint& a, const Mint& b) { return a.v <= b.v; }
+    friend bool operator>=(const Mint& a, const Mint& b) { return a.v >= b.v; }
+    Mint operator^(int64_t p) {
+        if(p < 0) return inv() ^ -p;
+        Mint a = *this, res{1}; while(p > 0) {
+            if(p & 1) res *= a;
+            p >>= 1; if(p > 0) a *= a;
+        } return res;
+    }
+};
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+using mint = Mint<MOD>;
+const int N = 400 + 5;
+mint a[N], b[N], dp[N][N], apow[N][N];
+
+int32_t main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int i, j, k, n, C;
+    cin >> n >> C;
+    for(i = 1; i <= n; i++)
+        cin >> a[i];
+    for(i = 1; i <= n; i++)
+        cin >> b[i];
+
+    for(i = 1; i <= n; i++)
+        for(k = 0; k <= C; k++)
+            for(mint z = a[i]; z <= b[i]; z++)
+                apow[i][k] += z ^ k;
+
+    dp[0][0] = 1;
+    for(i = 1; i <= n; i++)
+        for(j = 0; j <= C; j++)
+            for(k = 0; j+k <= C; k++)
+                dp[i][j+k] += dp[i-1][k] * apow[i][j];
+
+    cout << dp[n][C];
+}
