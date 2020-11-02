9fc2e3c551a290caf8d0c89916471ca76c6e6231
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-11-03 02:25:00 +0530

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/97510586

---
+++ codeforces/4/A.cpp
@@ -0,0 +1,24 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 03.11.2020 02:25:22
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+const int N = 2e5 + 5;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int i, n;
+    cin >> n;
+    cout << (n-2 and n&1^1? "YES" : "NO") << '\n';
+} // ~W
