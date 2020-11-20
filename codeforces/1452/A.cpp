fb726f6ba7fd5a063b1e0c1fbe503f81b77370a2
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-11-20 13:25:00 +0530

    Add solution for problem `A - Robot Program` Link: https://codeforces.com/contest/1452/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1452/submission/99009114

---
+++ codeforces/1452/A.cpp
@@ -0,0 +1,31 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 20.11.2020 13:19:50
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+const int N = 2e5 + 5;
+
+int ocoiek() {
+    int i, x, y;
+    cin >> x >> y;
+    if(x > y) swap(x, y);
+    cout << 2*y - (x != y) << '\n';
+
+    return 0;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int Q; for(cin >> Q; Q; Q--) ocoiek();
+} // ~W
