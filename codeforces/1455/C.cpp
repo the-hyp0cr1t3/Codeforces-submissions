0a82f645e739978d5491e0cd8e89afc8a5ee5b50
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-11-30 20:36:00 +0530

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/100013164

---
+++ codeforces/1455/C.cpp
@@ -0,0 +1,29 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 30.11.2020 20:24:35
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+const int N = 2e5 + 5;
+
+int ocoiek() {
+    int i, x, y, a, b;
+    cin >> x >> y;
+    cout << x-1 << ' ' << y << '\n';
+    return 0;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int Q; for(cin >> Q; Q; Q--) ocoiek();
+} // ~W
