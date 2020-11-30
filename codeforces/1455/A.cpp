57eb94e68179f32674ae28251da10d9dcf9184c1
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-11-30 20:06:00 +0530

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/99993613

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,30 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 30.11.2020 20:06:26
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+const int N = 2e5 + 5;
+
+int ocoiek() {
+    string x;
+    cin >> x;
+    cout << sz(x) << '\n';
+
+    return 0;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int Q; for(cin >> Q; Q; Q--) ocoiek();
+} // ~W
