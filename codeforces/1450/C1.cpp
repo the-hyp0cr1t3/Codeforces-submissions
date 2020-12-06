f4273d88b1df98af289d346e920d58c7cb4713fa
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-12-06 20:56:00 +0530

    Add solution for problem `C1 - Errich-Tac-Toe (Easy Version)` Link: https://codeforces.com/contest/1450/problem/C1 Tags: constructive algorithms, math, *2100 Ref: https://codeforces.com/contest/1450/submission/100552620

--- codeforces/1450/C1.cpp
+++ codeforces/1450/C1.cpp
@@ -1,6 +1,6 @@
 /**
  🍪 the_hyp0cr1t3
- 🍪 07.12.2020 16:04:39
+ 🍪 06.12.2020 20:34:51
 **/
 #ifdef W
 #include "k_II.h"
@@ -11,40 +11,37 @@ using namespace std;
 #define pb emplace_back
 #define sz(x) int(x.size())
 #define all(x) x.begin(), x.end()
+auto chmax = [](auto& A, auto&& B) { return B > A? A = B, true : false; };
+auto chmin = [](auto& A, auto&& B) { return B < A? A = B, true : false; };
 
 const int64_t DESPACITO = 2e18;
 const int INF = 2e9, MOD = 1e9+7;
 const int N = 2e5 + 5;
 
 int ocoiek() {
-    int i, j, k = 0, n;
+    int i, j, n;
     cin >> n;
     vector<string> a(n);
     for(auto& x: a) cin >> x;
 
-    array<array<vector<char*>, 2>, 3> pos;
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < n; j++) {
-            if(a[i][j] != '.') k++;
-            if(a[i][j] == 'X')
-                pos[(i+j)%3][0].pb(&a[i][j]);
-            if(a[i][j] == 'O')
-                pos[(i+j)%3][1].pb(&a[i][j]);
-        }
-    }
-
-    for(int p = 0; p < 3; p++) {
-        for(int q = 0; q < 3; q++) {
-            if(p ^ q and sz(pos[p][0]) + sz(pos[q][1]) <= k/3) {
-                for(auto x: pos[p][0]) *x = 'O';
-                for(auto x: pos[q][1]) *x = 'X';
-                for(auto x: a) cout << x << '\n';
-                return 0;
+    auto Try = [&](auto& b, int s) {
+        int cnt = 0;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < n; j++) {
+                if(b[i][j] != 'X') continue;
+                if((i+j)%3 == s) b[i][j] = 'O', cnt++;
             }
-        }
+        } return cnt;
+    };
+
+    int best = INF, y;
+    for(int z = 0; z < 3; z++) {
+        auto b = a;
+        if(chmin(best, Try(b, z))) y = z;
     }
 
-    assert(false);
+    Try(a, y);
+    for(auto& x: a) cout << x << '\n';
     return 0;
 }
 
