219c65b7ac23026a1e1c179962872fca3191ea4a
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2020-12-12 22:56:00 +0530

    Add solution for problem `E - Kamil and Making a Stream` Link: https://codeforces.com/contest/1230/problem/E Tags: math, number theory, trees, *2000 Ref: https://codeforces.com/contest/1230/submission/101048208

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-6 | [A - Selling Hamburgers](https://codeforces.com/contest/1431/problem/A) | [Kotlin](./codeforces/1431/A.kt) | `*special problem` `*800` | Dec/22/2020 00:43 | 
-5 | [C - Sonya and Robots](https://codeforces.com/contest/1004/problem/C) | [GNU C++17 (64)](./codeforces/1004/C.cpp) | `constructive algorithms` `implementation` `*1400` | Dec/22/2020 00:08 | 
-4 | [B - Move and Turn](https://codeforces.com/contest/1459/problem/B) | [GNU C++17 (64)](./codeforces/1459/B.cpp) | `dp` `math` `*1300` | Dec/19/2020 19:09 | 
-3 | [A - Red-Blue Shuffle](https://codeforces.com/contest/1459/problem/A) | [GNU C++17 (64)](./codeforces/1459/A.cpp) | `math` `probabilities` `*800` | Dec/19/2020 18:23 | 
-2 | [B - Glass Half Spilled](https://codeforces.com/contest/1458/problem/B) | [GNU C++17 (64)](./codeforces/1458/B.cpp) | `dp` `*2000` | Dec/19/2020 15:48 | 
-1 | [A - Row GCD](https://codeforces.com/contest/1458/problem/A) | [GNU C++17 (64)](./codeforces/1458/A.cpp) | `math` `number theory` `*1600` | Dec/19/2020 15:12 | 
+7 | [A - Selling Hamburgers](https://codeforces.com/contest/1431/problem/A) | [Kotlin](./codeforces/1431/A.kt) | `*special problem` `*800` | Dec/22/2020 00:43 | 
+6 | [C - Sonya and Robots](https://codeforces.com/contest/1004/problem/C) | [GNU C++17 (64)](./codeforces/1004/C.cpp) | `constructive algorithms` `implementation` `*1400` | Dec/22/2020 00:08 | 
+5 | [B - Move and Turn](https://codeforces.com/contest/1459/problem/B) | [GNU C++17 (64)](./codeforces/1459/B.cpp) | `dp` `math` `*1300` | Dec/19/2020 19:09 | 
+4 | [A - Red-Blue Shuffle](https://codeforces.com/contest/1459/problem/A) | [GNU C++17 (64)](./codeforces/1459/A.cpp) | `math` `probabilities` `*800` | Dec/19/2020 18:23 | 
+3 | [B - Glass Half Spilled](https://codeforces.com/contest/1458/problem/B) | [GNU C++17 (64)](./codeforces/1458/B.cpp) | `dp` `*2000` | Dec/19/2020 15:48 | 
+2 | [A - Row GCD](https://codeforces.com/contest/1458/problem/A) | [GNU C++17 (64)](./codeforces/1458/A.cpp) | `math` `number theory` `*1600` | Dec/19/2020 15:12 | 
+1 | [E - Kamil and Making a Stream](https://codeforces.com/contest/1230/problem/E) | [GNU C++17 (64)](./codeforces/1230/E.cpp) | `math` `number theory` `trees` `*2000` | Dec/12/2020 22:56 | 
---
+++ codeforces/1230/E.cpp
@@ -0,0 +1,132 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 12.12.2020 22:37:20
+**/
+#ifdef W
+#include "k_II.h"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+#define pb emplace_back
+#define sz(x) int(x.size())
+#define all(x) x.begin(), x.end()
+#include <ext/pb_ds/assoc_container.hpp>
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template<class T> class Y {
+    T f;
+public:
+    template<class U> explicit Y(U&& f): f(forward<U>(f)) {}
+    template<class... Args> decltype(auto) operator()(Args&&... args) {
+        return f(ref(*this), forward<Args>(args)...);
+    }
+}; template<class T> Y(T) -> Y<T>;
+
+template<const int& MOD>
+struct Modint {
+    using T = decay<decltype(MOD)>::type; T v;
+    Modint(int64_t v = 0) { if(v < 0) v = v % MOD + MOD; if(v >= MOD) v %= MOD; this->v = T(v); }
+    Modint(uint64_t v) { if (v >= MOD) v %= MOD; this->v = T(v); }
+    Modint(int v): Modint(int64_t(v)) {}
+    Modint(unsigned v): Modint(uint64_t(v)) {}
+    explicit operator int() const { return v; }
+    explicit operator int64_t() const { return v; }
+    explicit operator uint64_t() const { return v; }
+    friend istream& operator>>(istream& in, Modint& m) { int64_t v_; in >> v_; m = Modint(v_); return in; } 
+    friend ostream& operator<<(ostream& os, const Modint& m) { return os << m.v; }
+
+    static T inv(T a, T m) {
+        T g = m, x = 0, y = 1;
+        while(a != 0) {
+            T q = g / a;
+            g %= a; swap(g, a);
+            x -= q * y; swap(x, y);
+        } return x < 0? x + m : x;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif // x must be less than 2^32 * m
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x), quot, rem;
+        asm("divl %4\n" : "=a" (quot), "=d" (rem) : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    Modint inv() const { return Modint(inv(v, MOD)); }
+    Modint operator-() const { return Modint(v? MOD-v : 0); }
+    Modint& operator++() { v++; if(v == MOD) v = 0; return *this; }
+    Modint& operator--() { if(v == 0) v = MOD; v--; return *this; }
+    Modint operator++(int) { Modint a = *this; ++*this; return a; }
+    Modint operator--(int) { Modint a = *this; --*this; return a; }
+    Modint& operator+=(const Modint& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    Modint& operator-=(const Modint& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    Modint& operator*=(const Modint& o) { v = fast_mod(uint64_t(v) * o.v); return *this; }
+    Modint& operator/=(const Modint& o) { return *this *= o.inv(); }
+    friend Modint operator+(const Modint& a, const Modint& b) { return Modint(a) += b; }
+    friend Modint operator-(const Modint& a, const Modint& b) { return Modint(a) -= b; }
+    friend Modint operator*(const Modint& a, const Modint& b) { return Modint(a) *= b; }
+    friend Modint operator/(const Modint& a, const Modint& b) { return Modint(a) /= b; }
+    friend bool operator==(const Modint& a, const Modint& b) { return a.v == b.v; }
+    friend bool operator!=(const Modint& a, const Modint& b) { return a.v != b.v; }
+    friend bool operator<(const Modint& a, const Modint& b) { return a.v < b.v; }
+    friend bool operator>(const Modint& a, const Modint& b) { return a.v > b.v; }
+    friend bool operator<=(const Modint& a, const Modint& b) { return a.v <= b.v; }
+    friend bool operator>=(const Modint& a, const Modint& b) { return a.v >= b.v; }
+    Modint operator^(int64_t p) {
+        if(p < 0) return inv() ^ -p;
+        Modint a = *this, res{1}; while(p > 0) {
+            if(p & 1) res *= a;
+            p >>= 1; if(p > 0) a *= a;
+        } return res;
+    }
+};
+
+const int64_t DESPACITO = 2e18;
+const int INF = 2e9, MOD = 1e9+7;
+using mint = Modint<MOD>;
+const int N = 2e5 + 5;
+int64_t a[N];
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int i, n; mint ans;
+    cin >> n;
+    for(i = 0; i < n; i++)
+        cin >> a[i];
+    vector<vector<int>> g(n);
+    for(i = 0; i < n-1; i++) {
+        int u, v; cin >> u >> v; u--, v--;
+        g[u].pb(v); g[v].pb(u);
+    }
+    
+    unordered_map<int64_t, int> have;
+    Y([&](auto self, int v, int p) -> void {
+        unordered_map<int64_t, int> store;
+        store[a[v]] = 1; ans += a[v];
+        for(auto [gcd, cnt]: have) {
+            int64_t g = __gcd(gcd, a[v]);
+            store[g] += cnt;
+            ans += g * cnt;
+        } store.swap(have);
+        for(auto& x: g[v])
+            if(x ^ p) self(x, v);
+        have = std::move(store);
+    })(0, -1);
+
+    cout << ans;
+    
+} // ~W 
