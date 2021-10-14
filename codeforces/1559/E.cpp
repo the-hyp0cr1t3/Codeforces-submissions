d0b9c506054b591024dd15185a38a875f6281b59
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2021-10-14 16:56:00 +0530

    Add solution for problem `E - Mocha and Stars` Link: https://codeforces.com/contest/1559/problem/E Tags: combinatorics, dp, fft, math, number theory, *2200 Ref: https://codeforces.com/contest/1559/submission/131928272

---
+++ codeforces/1559/E.cpp
@@ -0,0 +1,112 @@
+/**
+ 🍪 the_hyp0cr1t3
+ 🍪 14.10.2021 16:16:04
+**/
+#ifdef W
+    #include <k_II.h>
+#else
+    #include <bits/stdc++.h>
+    using namespace std;
+#endif
+
+template<int MOD>
+struct Modint {
+    using T = typename decay<decltype(MOD)>::type; T v;
+    Modint(): v(0) {}
+    template<typename U, typename = enable_if_t<is_integral<U>::value>>
+    Modint(U v) { if(v < 0) v = v % MOD + MOD; if(v >= MOD) v %= MOD; this->v = static_cast<T>(v); }
+    template<typename U, typename = enable_if_t<is_integral<U>::value>>
+    explicit operator U() const { return static_cast<U>(v); }
+    friend istream& operator>>(istream& in, Modint& m) { int64_t v_; in >> v_; m = Modint(v_); return in; }
+    friend ostream& operator<<(ostream& os, const Modint& m) { return os << m.v; }
+
+    static T inv(T a, T m) {
+        T g = m, x = 0, y = 1;
+        while(a != 0) {
+            T q = g / a;
+            g %= a; swap(g, a);
+            x -= q * y; swap(x, y);
+        } return x < 0? x + m : x;
+    }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif // x must be less than 2^32 * m
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x), quot, rem;
+        asm("divl %4\n" : "=a" (quot), "=d" (rem) : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    Modint inv() const { return Modint(inv(v, MOD)); }
+    Modint operator-() const { return Modint(v? MOD-v : 0); }
+    Modint& operator++() { v++; if(v == MOD) v = 0; return *this; }
+    Modint& operator--() { if(v == 0) v = MOD; v--; return *this; }
+    Modint operator++(int) { Modint a = *this; ++*this; return a; }
+    Modint operator--(int) { Modint a = *this; --*this; return a; }
+    Modint& operator+=(const Modint& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    Modint& operator-=(const Modint& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    Modint& operator*=(const Modint& o) { v = fast_mod(uint64_t(v) * o.v); return *this; }
+    Modint& operator/=(const Modint& o) { return *this *= o.inv(); }
+    friend Modint operator+(const Modint& a, const Modint& b) { Modint res = a; res += b; return res; }
+    friend Modint operator-(const Modint& a, const Modint& b) { Modint res = a; res -= b; return res; }
+    friend Modint operator*(const Modint& a, const Modint& b) { Modint res = a; res *= b; return res; }
+    friend Modint operator/(const Modint& a, const Modint& b) { Modint res = a; res /= b; return res; }
+    friend bool operator==(const Modint& a, const Modint& b) { return a.v == b.v; }
+    friend bool operator!=(const Modint& a, const Modint& b) { return a.v != b.v; }
+    friend bool operator<(const Modint& a, const Modint& b) { return a.v < b.v; }
+    friend bool operator>(const Modint& a, const Modint& b) { return a.v > b.v; }
+    friend bool operator<=(const Modint& a, const Modint& b) { return a.v <= b.v; }
+    friend bool operator>=(const Modint& a, const Modint& b) { return a.v >= b.v; }
+    Modint operator^(int64_t p) {
+        if(p < 0) return inv() ^ -p;
+        Modint a = *this, res{1}; while(p > 0) {
+            if(p & 1) res *= a;
+            p >>= 1; if(p > 0) a *= a;
+        } return res;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    using mint = Modint<998244353>;
+
+    int i, j, n, m;
+    cin >> n >> m;
+    vector<int> l(n), r(n);
+    for(i = 0; i < n; i++)
+        cin >> l[i] >> r[i], ++r[i];
+
+    vector<mint> dp(m + 1), dp2(m + 1), cnt(m + 1);
+    for(int g = m; g > 0; g--) {
+        dp[0] = 1;
+        for(i = 0; i < n; i++) {
+            for(j = 0; j <= m; j += g) {
+                int _l = (j + l[i] + g - 1) / g * g;
+                int _r = (j + r[i] + g - 1) / g * g;
+
+                if(_l <= m)
+                    dp2[_l] += dp[j];
+
+                if(_r <= m)
+                    dp2[_r] -= dp[j];
+
+                if(j + g <= m)
+                    dp2[j + g] += dp2[j];
+
+                dp[j] = dp2[j];
+                dp2[j] = 0;
+            }
+        }
+
+
+        for(j = g; j <= m; j += g) {
+            cnt[g] -= cnt[j];
+            cnt[g] += dp[j];
+            dp[j] = 0;
+        }
+    }
+
+    cout << cnt[1];
+
+} // ~W
