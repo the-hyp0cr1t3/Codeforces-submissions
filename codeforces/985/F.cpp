5fb3d74e4427ff6e760ee370fa927e791a455e1f
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2022-01-07 00:07:00 +0530

    Add solution for problem `F - Isomorphic Strings` Link: https://codeforces.com/contest/985/problem/F Tags: hashing, strings, *2300 Ref: https://codeforces.com/contest/985/submission/141853788

--- codeforces/985/F.cpp
+++ codeforces/985/F.cpp
@@ -10,9 +10,9 @@
 #endif
 
 namespace Hashing {
-    constexpr int _mod = 1e9 + 123;
+    constexpr int _mod = 1e9+123;
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    static const int _base = uniform_int_distribution<int>(256, _mod - 2)(rng) | 1;
+    static const int _base = uniform_int_distribution<int>(256, _mod-2)(rng) | 1;
     using hash_t = pair<int, uint64_t>;
     vector<uint64_t> pow2{1};
 
@@ -27,25 +27,24 @@ namespace Hashing {
             n = s.size();
             assert(base < mod);
             suf1.resize(n + 1); suf2.resize(n + 1);
-            pow1.reserve(n + 1); pow2.reserve(n + 1);
-            while(pow1.size() <= n)
+            pow1.reserve(n+1); pow2.reserve(n+1);
+            while((int)pow1.size() <= n)
                 pow1.push_back(1LL * pow1.back() * base % mod);
-            while(pow2.size() <= n)
+            while((int)pow2.size() <= n)
                 pow2.push_back(pow2.back() * base);
-            for(int i = n - 1; ~i; i--) {
-                suf1[i] = (1ll * suf1[i + 1] * base + (s[i] == c)) % mod;
-                suf2[i] = suf2[i + 1] * base + (s[i] == c);
+            for(int i = n-1; ~i; i--) {
+                suf1[i] = (1ll * suf1[i+1] * base + (s[i] == c)) % mod;
+                suf2[i] = suf2[i+1] * base + (s[i] == c);
             }
         }
 
-        // hash [l, r) 0-based
         hash_t operator()(int l, int r) const {
-            int res1 = suf1[l] - 1ll * suf1[r] * pow1[r - l] % mod;
-            if(res1 < 0) res1 += mod;
-            uint64_t res2 = suf2[l] - suf2[r] * pow2[r - l];
+            int res1 = suf1[l] - 1ll * suf1[r+1] * pow1[r-l+1] % mod;
+            res1 = res1 < 0? res1 + mod : res1;
+            uint64_t res2 = suf2[l] - suf2[r+1] * pow2[r-l+1];
             return {res1, res2};
         }
-        hash_t operator()() const { return (*this)(0, n); }
+        hash_t operator()() const { return (*this)(0, n-1); }
     };
 
 }
@@ -79,8 +78,8 @@ int main() {
         for(auto _: {0, 1}) {
             for(i = 0; !bad and i < M; i++)
                 if(nxt[x][i] < x + len)
-                    bad |= hsh[s[nxt[x][i]] - 'a'](x, x + len)
-                            != hsh[s[y + nxt[x][i] - x] - 'a'](y, y + len);
+                    bad |= hsh[s[nxt[x][i]] - 'a'](x, x + len - 1)
+                            != hsh[s[y + nxt[x][i] - x] - 'a'](y, y + len - 1);
             swap(x, y);
         }
 
