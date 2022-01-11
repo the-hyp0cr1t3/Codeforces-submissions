2dab9bb65590dba29dfbbb6dc791018fa917beb2
the_hyp0cr1t3 <ghriday.bits@gmail.com>
2022-01-11 18:07:00 +0530

    Add solution for problem `D - X(or)-mas Tree` Link: https://codeforces.com/contest/1615/problem/D Tags: bitmasks, dfs and similar, dsu, graphs, trees, *2200 Ref: https://codeforces.com/contest/1615/submission/142369820

--- codeforces/1615/D.cpp
+++ codeforces/1615/D.cpp
@@ -9,32 +9,28 @@
     using namespace std;
 #endif
 
-// 0-indexed, (i, ¬i) represented by (i, i + n)
-class TwoSAT {
+template<class T> class Y {
+    T f;
+public:
+    template<class U> explicit Y(U&& f): f(forward<U>(f)) {}
+    template<class... Args> decltype(auto) operator()(Args&&... args) {
+        return f(ref(*this), forward<Args>(args)...);
+    }
+}; template<class T> Y(T) -> Y<T>;
+
+// 0-indexed, (i, ¬i) represented by (i << 1, i << 1 | 1)
+struct TwoSAT {
     int N;
-    vector<int> comp, order;
+    vector<int> comp, order, assignment;
     vector<vector<int>> g, rg;
 
-    int complement(int i) { return i < N? i + N : i - N; }
+    TwoSAT(int n): N(n), comp(n << 1, -1), g(n << 1), rg(n << 1), assignment(n) {}
 
-    void dfs(int v, int k, const vector<vector<int>>& gr) {
-        comp[v] = k;
-        for(auto& x: gr[v])
-            if(comp[x] == -1) dfs(x, k, gr);
-        if(k == -2) order.push_back(v);
-    };
-
-public:
-
-    TwoSAT(int n): N(n), comp(n << 1, -1), g(n << 1), rg(n << 1) {}
-
-    // p, q ∈ [0, 2n)
-    void implies(int p, int q) { g[p].push_back(q); rg[q].push_back(p); };                  // p → q
-    void add_truth(int p) { implies(complement(p), p); }                                    // p ≡ ¬p → p
-    void add_or (int p, int q) { implies(complement(p), q); implies(complement(q), p); };   // p ∨ q ≡ (¬p → q) ∧ (¬q → p)
-    // p, q ∈ [0, n)
-    void add_xor(int p, int q) { add_or(p, q); add_or(complement(p), complement(q)); };     // p ⊕ q ≡ (p ∨ q) ∧ (¬p ∨ ¬q)
-    void add_bicond(int p, int q) { add_xor(p, complement(q)); };                           // p ↔ q ≡ (p → q) ∧ (q → p) ≡ (p ⊕ ¬q)
+    void implies(int p, int q) { g[p].push_back(q); rg[q].push_back(p); };    // p → q
+    void add_or (int p, int q) { implies(p ^ 1, q); implies(q ^ 1, p); };     // p ∨ q ≡ (¬p → q) ∧ (¬q → p)
+    void add_xor(int p, int q) { add_or(p, q); add_or(p ^ 1, q ^ 1); };       // p ⊕ q ≡ (p ∨ q) ∧ (¬p ∨ ¬q)
+    void add_bicond(int p, int q) { add_xor(p, q ^ 1); };                     // p ↔ q ≡ (p → q) ∧ (q → p) ≡ (p ⊕ ¬q)
+    void add_truth(int p) { implies(p ^ 1, p); }                              // p ≡ ¬p → p
 
     bool is_sat() {
         order.reserve(N << 1);
@@ -48,18 +44,23 @@ public:
         for(auto& v: order)
             if(comp[v] == -1) dfs(v, _++, rg);
 
-        for(int i = 0; i < N; i++)
-            if(comp[i] == comp[i + N]) return false;
+        bool bad = false;
+        for(int i = 0; i < N; i++) {
+            if(comp[i << 1] == comp[i << 1 | 1]) bad = true;
+            assignment[i] = comp[i << 1] > comp[i << 1 | 1];
+        }
 
-        return true;
+        return !bad;
     }
 
-    vector<int> get_assignment() {
-        vector<int> assn(N);
-        for(int i = 0; i < N; i++)
-            assn[i] = comp[i] > comp[i + N];
-        return assn;
-    }
+    bool get(int i) { return assignment[i]; }
+
+    void dfs(int v, int k, const vector<vector<int>>& gr) {
+        comp[v] = k;
+        for(auto& x: gr[v])
+            if(comp[x] == -1) dfs(x, k, gr);
+        if(k == -2) order.push_back(v);
+    };
 
 };
 
@@ -74,25 +75,34 @@ int main() {
         int i, n, m;
         cin >> n >> m;
         TwoSAT sat(n);
-        vector<tuple<int, int, int>> edges(n - 1);
-        for(auto& [u, v, w]: edges) {
-            cin >> u >> v >> w; --u, --v;
-            if(~w) __builtin_popcount(w) & 1? sat.add_xor(u, v) : sat.add_bicond(u, v);
+        sat.add_truth(0 << 1);
+
+        vector<vector<pair<int, int>>> g(n);
+        for(i = 0; i < n - 1; i++) {
+            int u, v, w; cin >> u >> v >> w;
+            g[--u].emplace_back(--v, w);
+            g[v].emplace_back(u, w);
+            if(~w) __builtin_popcount(w) & 1? sat.add_xor(u << 1, v << 1)
+                                                : sat.add_bicond(u << 1, v << 1);
         }
 
         while(m--) {
-            int a, b, p; cin >> a >> b >> p; --a; --b;
-            p? sat.add_xor(a, b) : sat.add_bicond(a, b);
+            int a, b, p; cin >> a >> b >> p;
+            --a; --b;
+            p? sat.add_xor(a << 1, b << 1) : sat.add_bicond(a << 1, b << 1);
         }
 
         if(!sat.is_sat())
             return cout << "nO" << '\n', 0;
 
-        auto assn = sat.get_assignment();
-
         cout << "yEs" << '\n';
-        for(auto& [u, v, w]: edges)
-            cout << u + 1 << ' ' << v + 1 << ' ' << (~w? w : assn[u] ^ assn[v]) << '\n';
+        Y([&](auto dfs, int v, int p) -> void {
+            for(auto& [x, w]: g[v]) if(x ^ p) {
+                cout << v + 1 << ' ' << x + 1 << ' '
+                        << (~w? w : sat.get(v) ^ sat.get(x)) << '\n';
+                dfs(x, v);
+            }
+        })(0, -1);
 
         return 0;
     }();
